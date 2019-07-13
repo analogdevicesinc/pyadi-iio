d175d4fe7e67f1415fdec3ec9e9ab4e06c396fce
Travis F. Collins <travis.collins@analog.com>
2019-07-12 18:42:28 -0700

    Readme updates
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyadi",
-    version="0.0.1",
+    name="pyadi-iio",
+    version="0.0.2",
     author="Travis Collins",
     author_email="travis.collins@analog.com",
     description="Interfaces to stream data from ADI hardware",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/analogdevicesinc/pyadi",
+    url="https://github.com/analogdevicesinc/pyadi-iio",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: ADI BSD",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
 )
