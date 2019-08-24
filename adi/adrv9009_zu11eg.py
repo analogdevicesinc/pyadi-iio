a07eb4b9fdc3771535b0f1c1c3cbb3573527f435
Travis F. Collins <travis.collins@analog.com>
2019-08-24 10:09:01 -0700

    Remove unused imports
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- adi/adrv9009_zu11eg.py
+++ adi/adrv9009_zu11eg.py
@@ -31,8 +31,6 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from adi.rx_tx import rx_tx
-from adi.context_manager import context_manager
 from adi.adrv9009 import adrv9009
 
 
