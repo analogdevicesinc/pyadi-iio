a86f7405c5ac9120d5e0ba3b0b3600e0ad15b8f5
Travis F. Collins <travis.collins@analog.com>
2019-08-23 20:00:00 -0700

    Add initial support for ADRV9009-ZU11EG
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- adi/attribute.py
+++ adi/attribute.py
@@ -33,29 +33,44 @@
 
 
 class attribute:
-    def set_iio_attr_str(self, channel_name, attr_name, output, value):
-        channel = self.ctrl.find_channel(channel_name, output)
+    def set_iio_attr_str(self, channel_name, attr_name, output, value, ctrl=None):
+        if ctrl:
+            channel = ctrl.find_channel(channel_name, output)
+        else:
+            channel = self.ctrl.find_channel(channel_name, output)
         try:
             channel.attrs[attr_name].value = str(value)
         except Exception as ex:
             raise ex
 
-    def set_iio_attr(self, channel_name, attr_name, output, value):
-        channel = self.ctrl.find_channel(channel_name, output)
+    def set_iio_attr(self, channel_name, attr_name, output, value, ctrl=None):
+        if ctrl:
+            channel = ctrl.find_channel(channel_name, output)
+        else:
+            channel = self.ctrl.find_channel(channel_name, output)
         try:
             channel.attrs[attr_name].value = str(int(value))
         except Exception as ex:
             raise ex
 
-    def get_iio_attr(self, channel_name, attr_name, output):
-        channel = self.ctrl.find_channel(channel_name, output)
+    def get_iio_attr(self, channel_name, attr_name, output, ctrl=None):
+        if ctrl:
+            channel = ctrl.find_channel(channel_name, output)
+        else:
+            channel = self.ctrl.find_channel(channel_name, output)
         return channel.attrs[attr_name].value
 
-    def set_iio_dev_attr_str(self, attr_name, value):
+    def set_iio_dev_attr_str(self, attr_name, value, ctrl=None):
         try:
-            self.ctrl.attrs[attr_name].value = str(value)
+            if ctrl:
+                ctrl.attrs[attr_name].value = str(value)
+            else:
+                self.ctrl.attrs[attr_name].value = str(value)
         except Exception as ex:
             raise ex
 
-    def get_iio_dev_attr(self, attr_name):
-        return self.ctrl.attrs[attr_name].value
+    def get_iio_dev_attr(self, attr_name, ctrl=None):
+        if ctrl:
+            return ctrl.attrs[attr_name].value
+        else:
+            return self.ctrl.attrs[attr_name].value
