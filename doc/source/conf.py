5de6b580fe70540c27d27dc0be6f8dd13388347e
Travis F. Collins <travis.collins@analog.com>
2019-09-04 15:12:41 -0700

    Bump to v0.0.3
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,11 +22,11 @@ sys.setrecursionlimit(1500)
 # -- Project information -----------------------------------------------------
 
 project = "Analog Devices Hardware Python Interfaces"
-copyright = "2019, Travis Collins"
+copyright = "2019, Analog Devices, Inc"
 author = "Travis Collins"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.0.3"
 
 
 # -- General configuration ---------------------------------------------------
