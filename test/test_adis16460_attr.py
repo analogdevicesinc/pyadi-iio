aa4627c2cc2d56d624bf5f275c446768c5e50823
Travis F. Collins <travis.collins@analog.com>
2019-09-05 17:08:48 -0700

    Add iio-scanner to automatically find correct hardware for tests on the network
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_adis16460_attr.py
+++ test/test_adis16460_attr.py
@@ -1,4 +1,5 @@
 import random
+import test.iio_scanner as iio_scanner
 
 import iio
 
@@ -6,33 +7,27 @@ import numpy as np
 import pytest
 from adi import adis16460
 
+URI = "ip:analog"
 dev_checked = False
 found_dev = False
 
-URI = 'ip:analog'
 
-def check_adis16460():
-    # Try auto discover
-    try:
-        iio.Context(URI)
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
-def check_dev():
+def check_dev(name):
     global dev_checked
     global found_dev
     if not dev_checked:
-        found_dev = check_adis16460()
+        found_dev, board = iio_scanner.find_device(name)
+        if found_dev:
+            global URI
+            URI = board.uri
         dev_checked = True
     return found_dev
 
 
-@pytest.mark.skipif(not check_dev(), reason="ADIS16460 not connected")
+@pytest.mark.skipif(not check_dev("packrf"), reason="ADIS16460 not connected")
 def testADIS16460_SensorData():
     # See if we can get non-zero data from ADC
+    global URI
     s = adis16460(uri=URI)
     data = s.rx()
     for k in data:
@@ -43,9 +38,10 @@ def testADIS16460_SensorData():
 scalar_properties = [("sample_rate", 1, 4096, 0)]
 
 
-@pytest.mark.skipif(not check_dev(), reason="ADIS16460 not connected")
+@pytest.mark.skipif(not check_dev("packrf"), reason="ADIS16460 not connected")
 @pytest.mark.parametrize("attr, start, stop, tol", scalar_properties)
 def test_adis16460_attribute_single_value(attr, start, stop, tol):
+    global URI
     sdr = adis16460(uri=URI)
     # Pick random number in operational range
     nums = []
