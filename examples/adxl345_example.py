69f7682b45f1368486184ed28ff0bff2c9755bed
mark thoren <mark.thoren@analog.com>
2019-12-23 07:13:10 -0800

    Add ADXL345 support AD7124: Add type annotation to pass mypy
    Revert AD7124 changes
    
    Update to use rx(), remove to_g function
    
    read out raw attribute
    
    Signed-off-by: mark thoren <mark.thoren@analog.com>

--- examples/adxl345_example.py
+++ examples/adxl345_example.py
@@ -31,27 +31,37 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from adi.ad9361 import *
+import time
 
-from adi.fmcomms5 import *
+import adi
 
-from adi.ad9371 import *
+# Set up ADXL345
+myacc = adi.adxl345(uri="ip:192.168.1.232")
+myacc.rx_output_type = "SI"
+myacc.rx_buffer_size = 4
+myacc.rx_enabled_channels = [0, 1, 2]
 
-from adi.adrv9009 import *
+sfa = myacc.sampling_frequency_available
+print("Sampling frequencies available:")
+print(sfa)
 
-from adi.adrv9009_zu11eg import *
+print("\nX acceleration: " + str(myacc.accel_x.raw))
+print("Y acceleration: " + str(myacc.accel_y.raw))
+print("Z acceleration: " + str(myacc.accel_z.raw))
 
-from adi.ad9680 import *
+print("\nSample rate: " + str(myacc.sampling_frequency))
 
-from adi.ad9144 import *
+print("Setting sample rate to 12.5 sps...")
+myacc.sampling_frequency = 12.5
+time.sleep(0.25)
 
-from adi.daq2 import *
+print("new sample rate: " + str(myacc.sampling_frequency))
+myacc.sampling_frequency = 100.0  # Set back to default
+print("\nData using unbuffered rx(), SI (m/s^2):")
+print(myacc.rx())
 
-from adi.adis16460 import *
+myacc.rx_output_type = "raw"
+print("\nData using unbuffered rx(), raw:")
+print(myacc.rx())
 
-from adi.ad7124 import *
-
-from adi.adxl345 import *
-
-__version__ = "0.0.5"
-name = "Analog Devices Hardware Interfaces"
+del myacc
