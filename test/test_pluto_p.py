de8a339ade37541fac386636a6a7a4d152c79b1b
Jimenez <Kister.Jimenez@analog.com>
2020-02-06 15:11:54 +0800

    -Added Parametrized test for FMComms5. Remove non-parametrized test_fmcomms5. -Added cyclic buffer testing and cyclic buffer exception testing routines and fixtures in conftest. -Fixed the multiline strings formatting in the Thrown exceptions. -Added DDS Loopback testing routine and fixture in conftest.

--- test/test_pluto_p.py
+++ test/test_pluto_p.py
@@ -48,6 +48,48 @@ def test_pluto_tx_data(test_dma_tx, classname, hardware, channel):
 #########################################
 @pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
 @pytest.mark.parametrize("channel", [0])
+@pytest.mark.parametrize(
+    "param_set",
+    [
+        dict(
+            tx_lo=1000000000,
+            rx_lo=1000000000,
+            gain_control_mode_chan0="slow_attack",
+            tx_hardwaregain_chan0=-20,
+            sample_rate=4000000,
+        )
+    ],
+)
+def test_pluto_cyclic_buffers(
+    test_cyclic_buffer, classname, hardware, channel, param_set
+):
+    test_cyclic_buffer(classname, hardware, channel, param_set)
+
+
+#########################################
+@pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
+@pytest.mark.parametrize("channel", [0])
+@pytest.mark.parametrize(
+    "param_set",
+    [
+        dict(
+            tx_lo=1000000000,
+            rx_lo=1000000000,
+            gain_control_mode_chan0="slow_attack",
+            tx_hardwaregain_chan0=-20,
+            sample_rate=4000000,
+        )
+    ],
+)
+def test_pluto_cyclic_buffers_exception(
+    test_cyclic_buffer_exception, classname, hardware, channel, param_set
+):
+    test_cyclic_buffer_exception(classname, hardware, channel, param_set)
+
+
+#########################################
+@pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
+@pytest.mark.parametrize("channel", [0])
 def test_pluto_loopback(test_dma_loopback, classname, hardware, channel):
     test_dma_loopback(classname, hardware, channel)
 
@@ -70,6 +112,28 @@ def test_pluto_loopback(test_dma_loopback, classname, hardware, channel):
 @pytest.mark.parametrize("sfdr_min", [40])
 def test_pluto_sfdr(test_sfdr, classname, hardware, channel, param_set, sfdr_min):
     test_sfdr(classname, hardware, channel, param_set, sfdr_min)
+
+
+#########################################
+@pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
+@pytest.mark.parametrize(
+    "param_set",
+    [
+        dict(
+            tx_lo=1000000000,
+            rx_lo=1000000000,
+            gain_control_mode_chan0="slow_attack",
+            tx_hardwaregain_chan0=-30,
+            sample_rate=4000000,
+            dds_scales=[0.6, 0, 0.6, 0],
+        )
+    ],
+)
+@pytest.mark.parametrize("peak_min", [-40])
+def test_pluto_dds_loopback(
+    test_dds_loopback, classname, hardware, param_set, peak_min
+):
+    test_dds_loopback(classname, hardware, param_set, peak_min)
 
 
 #########################################
