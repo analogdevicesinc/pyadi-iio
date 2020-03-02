f99908d08a4535ffdb5fa027575b786ba036b1d5
slash <bancisor.mihai@gmail.com>
2020-03-02 19:36:43 +0200

    updates

--- examples/adrv9009_som_multi.py
+++ examples/adrv9009_som_multi.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 from scipy import signal
+import csv
 
 
 def measure_phase_and_delay(chan0, chan1, window=None):
@@ -45,13 +46,16 @@ buff_size = 2 ** 14
 
 # Create radio
 # def run_dev():
-master = "ip:192.168.86.51"
-slave = "ip:192.168.86.39"
+master = "ip:192.168.1.60"
+slave = "ip:192.168.1.61"
+
+#master = "ip:10.48.65.100"
+#slave = "ip:10.48.65.107"
 
 print("--Connecting to devices")
 multi = adi.adrv9009_zu11eg_multi(master, [slave])
 # multi._dma_show_arming = True
-multi.rx_buffer_size = 2 ** 14
+multi.rx_buffer_size = 2 ** 10
 
 # Configure LOs
 multi.master.trx_lo = 1000000000
@@ -63,31 +67,57 @@ for slave in multi.slaves:
 
 multi.master.dds_single_tone(30000, 0.8)
 
-for r in range(10):
+log = [[], [], []]
+
+for r in range(3):
     time.sleep(3)
     # Collect data
     print("Pulling buffers")
     x = multi.rx()
-    print("Same Chip       ", measure_phase(x[0], x[1]))
-    print("Across Chip     ", measure_phase(x[0], x[2]))
-
-    print("Across SOMS (AB)", measure_phase(x[0], x[4]))
+    rx1rx2 = measure_phase(x[0], x[1])
+    rx1rx3 = measure_phase(x[0], x[2])
+    rx1rx5 = measure_phase(x[0], x[4])
+    
+    log[0].append(rx1rx2)
+    log[1].append(rx1rx3)
+    log[2].append(rx1rx5)
+    
     print("###########")
-    (p, s) = measure_phase_and_delay(x[0], x[1])
-    print("Same Chip Sample delay       :", s)
-    (p, s) = measure_phase_and_delay(x[0], x[2])
-    print("Across Chips Sample delay    :", s)
-    (p, s) = measure_phase_and_delay(x[0], x[4])
-    print("Across SOMS (AB) Sample delay:", s)
-    # print("Phase delay: ",p)
-    print("------------------")
+    print("Same Chip       ", rx1rx2)
+    print("Across Chip     ", rx1rx3)
+
+    print("Across SOMS (AB)", rx1rx5)
+    print("###########")
+#    (p, s) = measure_phase_and_delay(x[0], x[1])
+#    print("Same Chip Sample delay       :", s)
+#    (p, s) = measure_phase_and_delay(x[0], x[2])
+#    print("Across Chips Sample delay    :", s)
+#    (p, s) = measure_phase_and_delay(x[0], x[4])
+#    print("Across SOMS (AB) Sample delay:", s)
+#    # print("Phase delay: ",p)
+#    print("------------------")
 
     plt.clf()
-    plt.plot(x[0], label="Chan1 SOM A")
-    plt.plot(x[2], label="Chan2 SOM A")
-    plt.plot(x[4], label="Chan1 SOM B")
+    plt.plot(x[0][:100], label="Chan1 SOM A")
+    plt.plot(x[2][:100], label="Chan2 SOM A")
+    plt.plot(x[4][:100], label="Chan1 SOM B")
     plt.legend()
     plt.draw()
     plt.pause(0.1)
-
-plt.show()
+print(log)
+fields = []
+fields.append(sum(log[0]) / len(log[0]))
+fields.append(min(log[0]))
+fields.append(max(log[0]))
+fields.append(sum(log[1]) / len(log[1]))
+fields.append(min(log[1]))
+fields.append(max(log[1]))
+fields.append(sum(log[2]) / len(log[2]))
+fields.append(min(log[2]))
+fields.append(max(log[2]))
+with open(r'log.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(fields)
+plt.show(block=False)
+plt.pause(2)
+plt.close()
