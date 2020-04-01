b18fb3acb9d50c3aa332934bf5856bdd886b32de
Travis F. Collins <travis.collins@analog.com>
2020-03-31 17:58:06 -0700

    Autogenerate device doc
    Added script to autogenerate doc for device classes using sphinx-apidoc
    
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- doc/source/devices/index.rst
+++ doc/source/devices/index.rst
@@ -1,12 +1,28 @@
 Supported Devices
-==================
+=================
+
+
+
 
 .. toctree::
-   :maxdepth: 1
 
-   ad9361
-   ad9371
-   adrv9009
-   daq2
-   adis16460
-   ad7124
+   adi.ad5627
+   adi.ad5686
+   adi.ad7124
+   adi.ad9094
+   adi.ad9144
+   adi.ad9152
+   adi.ad936x
+   adi.ad9371
+   adi.ad9680
+   adi.adis16460
+   adi.adis16507
+   adi.adrv9009
+   adi.adrv9009_zu11eg
+   adi.adxl345
+   adi.daq2
+   adi.daq3
+   adi.fmclidar1
+   adi.fmcomms5
+   adi.ltc2983
+
---
+++ doc/update_devs.sh
@@ -0,0 +1,27 @@
+rm source/devices/adi.*
+rm source/devices/modules.rst
+sphinx-apidoc -T -e -o source/devices ../adi
+
+# Remove adi. and modules strings
+find source/devices -mindepth 1 -type f -exec sed -i '1 s/adi\.//g' {} \;
+find source/devices -mindepth 1 -type f -exec sed -i 's/\ module//g' {} \;
+
+# Remove classes we shouldn't document
+list="obs attribute context_manager dds rx_tx"
+for val in $list; do
+    echo $val
+    sed -i "/$val/d" source/devices/adi.rst
+    rm source/devices/adi.${val}.rst
+done
+
+# Remove extra text
+sed -i '/Module.*/q' source/devices/adi.rst
+sed -i '/Module/d' source/devices/adi.rst
+
+sed -i 's/adi\ package/Supported\ Devices/' source/devices/adi.rst
+sed -i 's/Submodules//' source/devices/adi.rst
+sed -i 's/----------//' source/devices/adi.rst
+sed -i 's/===========/=================/' source/devices/adi.rst
+mv source/devices/adi.rst source/devices/index.rst
+
+make html
