3d721d10555f05fa60c8e55fb6c732e81d7bcdec
Travis F. Collins <travis.collins@analog.com>
2020-06-25 17:21:38 -0700

    Add paramset to adrv9002 tests
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_adrv9002_p.py
+++ test/test_adrv9002_p.py
@@ -38,7 +38,22 @@ def test_adrv9002_rx_data(test_dma_rx, classname, hardware, channel):
 @pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
 @pytest.mark.parametrize("channel", [0])
 @pytest.mark.parametrize(
-    "param_set", [dict(),],
+    "param_set",
+    [
+        dict(
+            tx1_lo=1000000000,
+            rx1_lo=1000000000,
+            interface_gain_chan0="-12dB",
+            interface_gain_chan1="-12dB",
+            rx_ensm_mode_chan0="rf_enabled",
+            rx_ensm_mode_chan1="rf_enabled",
+            tx_hardwaregain_chan0=-20,
+            tx_ensm_mode_chan0="rf_enabled",
+            tx_cyclic_buffer=True,
+        )
+    ],
 )
-def test_adrv9002_cw_loopback(test_cw_loopback, classname, hardware, channel, param_set):
+def test_adrv9002_cw_loopback(
+    test_cw_loopback, classname, hardware, channel, param_set
+):
     test_cw_loopback(classname, hardware, channel, param_set)
