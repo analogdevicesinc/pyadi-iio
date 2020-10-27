d8e9bf26c918e9f2d0018da9db4db40c31da826c
Michael Hennerich <michael.hennerich@analog.com>
2020-10-27 12:25:08 +0100

    examples: adrv9009_som_multi.py: Update for multi-topology SoM+FMCOMMS8 systems
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- examples/adrv9009_som_multi.py
+++ examples/adrv9009_som_multi.py
@@ -1,5 +1,6 @@
 # type: ignore
 
+import csv
 import time
 
 import adi
@@ -7,7 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 from scipy import signal
-import csv
 
 
 def measure_phase_and_delay(chan0, chan1, window=None):
@@ -45,77 +45,111 @@ def measure_phase(chan0, chan1):
 buff_size = 2 ** 14
 
 # Create radio
-# def run_dev():
-master = "ip:192.168.86.35"
-slave = "ip:192.168.86.44"
+primary = "ip:10.44.3.39"
+secondary = "ip:10.44.3.38"
 
-master_jesd = adi.jesd(master)
-slave_jesd = adi.jesd(slave)
+lo_freq = 1000000000
+dds_freq = 1234567
+
+primary_jesd = adi.jesd(primary)
+secondary_jesd = adi.jesd(secondary)
 
 print("--Connecting to devices")
-multi = adi.adrv9009_zu11eg_multi(master, [slave], master_jesd, [slave_jesd])
-# multi = adi.adrv9009_zu11eg_multi(master, [slave])
-# multi._dma_show_arming = True
+multi = adi.adrv9009_zu11eg_multi(
+    primary, [secondary], primary_jesd, [secondary_jesd], fmcomms8=True
+)
+
+multi._dma_show_arming = False
+multi._jesd_show_status = True
 multi.rx_buffer_size = 2 ** 10
 
-# Configure LOs
-multi.master.trx_lo = 1000000000
-multi.master.trx_lo_chip_b = 1000000000
+multi.primary.rx_enabled_channels = [0, 2, 4, 6]
 
-for slave in multi.slaves:
-    slave.trx_lo = 1000000000
-    slave.trx_lo_chip_b = 1000000000
+for secondary in multi.secondaries:
+    secondary.rx_enabled_channels = [0, 2, 4, 6]
 
-multi.master.dds_single_tone(30000, 0.8)
+multi.set_trx_lo_frequency(999999990)
+multi.primary.dds_single_tone(dds_freq, 0.8)
 
-log = [[], [], []]
+log = [[], [], [], [], []]
 
-for r in range(3):
-    time.sleep(3)
-    # Collect data
+N = 8
+C = 5
+R = 32
+
+plot_time = True
+
+rx = np.zeros([C, N])
+rx_m = np.zeros([C, R])
+rx_v = np.zeros([C, R])
+
+chan_desc = [
+    "Across Chip (A)",
+    "Across FMC8 (A)",
+    "Across Chip (B)",
+    "Across FMC8 (B)",
+    "Across SoM (AB)",
+]
+
+for r in range(R):
     print("Pulling buffers")
-    x = multi.rx()
-    rx1rx2 = measure_phase(x[0], x[1])
-    rx1rx3 = measure_phase(x[0], x[2])
-    rx1rx5 = measure_phase(x[0], x[4])
-    
-    log[0].append(rx1rx2)
-    log[1].append(rx1rx3)
-    log[2].append(rx1rx5)
-    
-    print("###########")
-    print("Same Chip       ", rx1rx2)
-    print("Across Chip     ", rx1rx3)
+    multi._rx_initialized = False
 
-    print("Across SOMS (AB)", rx1rx5)
+    # [0, 2, 4, 6][0, 2, 4, 6]
+    # [0, 1, 2 ,3, 4, 5, 6, 7]
+    for i in range(N):
+        x = multi.rx()
+        rx[0][i] = measure_phase(x[0], x[1])
+        rx[1][i] = measure_phase(x[0], x[2])
+        rx[2][i] = measure_phase(x[4], x[5])
+        rx[3][i] = measure_phase(x[4], x[6])
+        rx[4][i] = measure_phase(x[0], x[4])
+
+    for i in range(C):
+        rx_m[i][r] = np.mean(rx[i])
+        rx_v[i][r] = np.var(rx[i])
+        if rx_v[i][r] > 10:
+            plot_time = True
+        else:
+            plot_time = False
+
+        log[i].append(rx_m[i])
+
     print("###########")
-#    (p, s) = measure_phase_and_delay(x[0], x[1])
-#    print("Same Chip Sample delay       :", s)
-#    (p, s) = measure_phase_and_delay(x[0], x[2])
-#    print("Across Chips Sample delay    :", s)
-#    (p, s) = measure_phase_and_delay(x[0], x[4])
-#    print("Across SOMS (AB) Sample delay:", s)
-#    # print("Phase delay: ",p)
-#    print("------------------")
+    for i in range(C):
+        print("%s:\t %f" % (chan_desc[i], rx_m[i][r]))
+    print("###########")
+
+    if plot_time:
+        plt.clf()
+        plt.plot(x[0][:1000], label="Chan0 SOM A")
+        plt.plot(x[1][:1000], label="Chan2 SOM A")
+        plt.plot(x[2][:1000], label="Chan4 SOM A FMC8")
+        plt.plot(x[4][:1000], label="Chan0 SOM B")
+        plt.plot(x[6][:1000], label="Chan4 SOM B FMC8")
+        plt.legend()
+        plt.draw()
+        plt.pause(5)
 
     plt.clf()
-    plt.plot(x[0][:100], label="Chan1 SOM A")
-    plt.plot(x[2][:100], label="Chan2 SOM A")
-    plt.plot(x[4][:100], label="Chan1 SOM B")
+    x = np.array(range(0, r + 1))
+
+    for i in range(C):
+        plt.errorbar(x, rx_m[i][x], yerr=rx_v[i][x], label=chan_desc[i])
+
+    plt.xlim([-1, x[-1] + 1])
+    plt.xlabel("Measurement Index")
+    plt.ylabel("Phase Difference (Degrees)")
     plt.legend()
     plt.draw()
     plt.pause(0.1)
+
 print(log)
 fields = []
-fields.append(sum(log[0]) / len(log[0]))
-fields.append(min(log[0]))
-fields.append(max(log[0]))
-fields.append(sum(log[1]) / len(log[1]))
-fields.append(min(log[1]))
-fields.append(max(log[1]))
-fields.append(sum(log[2]) / len(log[2]))
-fields.append(min(log[2]))
-fields.append(max(log[2]))
+for i in range(C):
+    fields.append(np.sum(log[i]) / len(log[i]))
+    fields.append(np.min(log[i]))
+    fields.append(np.max(log[i]))
 with open(r'log.csv', 'a') as f:
     writer = csv.writer(f)
     writer.writerow(fields)
