74d93f3a1040b5f540f0fdec509bd45ef63f4ccc
Travis F. Collins <travis.collins@analog.com>
2020-11-16 17:45:33 -0800

    Initial doc for testing fixtures
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ doc/source/dev/test.rst
@@ -0,0 +1,9 @@
+Test Library
+=================
+
+Functions used by test fixtures for evaluating attributes, driver state, and buffers.
+
+.. automodule:: test.conftest
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- test/conftest.py
+++ test/conftest.py
@@ -11,7 +11,28 @@ import pytest
 
 
 def attribute_single_value(uri, classname, attr, start, stop, step, tol, repeats=1):
-    # bi = BoardInterface(classname, devicename)
+    """ attribute_single_value: Write and read back integer class property
+            This is performed a defined number of times and the value written
+            is randomly determined based in input parameters
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            start: type=integer
+                Lower bound of possible values attribute can be
+            stop: type=integer
+                Upper bound of possible values attribute can be
+            step: type=integer
+                Difference between successive values attribute can be
+            tol: type=integer
+                Allowable error of written value compared to read back value
+            repeats: type=integer
+                Number of random values to tests. Generated from uniform distribution
+    """
     # Pick random number in operational range
     numints = int((stop - start) / step)
     for _ in range(repeats):
@@ -22,17 +43,46 @@ def attribute_single_value(uri, classname, attr, start, stop, step, tol, repeats
 
 
 def attribute_single_value_str(uri, classname, attr, val, tol):
-    # bi = BoardInterface(classname, devicename)
+    """ attribute_single_value_str: Write and read back string class property
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            val: type=string
+                Value to write and read back from attribute
+            tol: type=integer
+                Allowable error of written value compared to read back value
+    """
     # Check hardware
     assert dev_interface(uri, classname, str(val), attr, tol) <= tol
 
 
 def attribute_single_value_pow2(uri, classname, attr, max_pow, tol, repeats=1):
-    # bi = BoardInterface(classname, devicename)
+    """ attribute_single_value_pow2: Write and read back integer class property
+            where the integer is a power of 2. This is performed a defined
+            number of times and the value written is randomly determined based
+            in input parameters
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            max_pow: type=integer
+                Largest power of 2 attribute allow to be
+            tol: type=integer
+                Allowable error of written value compared to read back value
+            repeats: type=integer
+                Number of random values to tests. Generated from uniform distribution
+    """
     # Pick random number in operational range
-    nums = []
-    for k in range(0, max_pow):
-        nums.append(2 ** k)
+    nums = [2 ** k for k in range(max_pow)]
     for _ in range(repeats):
         ind = random.randint(0, len(nums) - 1)
         val = nums[ind]
@@ -41,7 +91,24 @@ def attribute_single_value_pow2(uri, classname, attr, max_pow, tol, repeats=1):
 
 
 def attribute_multipe_values(uri, classname, attr, values, tol, repeats=1):
-    # bi = BoardInterface(classname, devicename)
+    """ attribute_multipe_values: Write and read back multiple class properties
+            in a loop where all values are pre-defined. This is performed a defined
+            number of times.
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            values: type=list
+                A list of values to write and check as attributes
+            tol: type=integer
+                Allowable error of written value compared to read back value
+            repeats: type=integer
+                Number of times to repeatedly write values
+    """
     for _ in range(repeats):
         for val in values:
             if isinstance(val, str):
@@ -53,7 +120,27 @@ def attribute_multipe_values(uri, classname, attr, values, tol, repeats=1):
 def attribute_multipe_values_with_depends(
     uri, classname, attr, depends, values, tol, repeats=1
 ):
-    # bi = BoardInterface(classname, devicename)
+    """ attribute_multipe_values_with_depends: Write and read back multiple class
+            properties in a loop where all values are pre-defined, where a set of
+            dependent attributes are written first. This is performed a defined
+            number of times.
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            depends: type=list
+                A list of dependent values to write and check as attributes
+            values: type=list
+                A list of values to write and check as attributes
+            tol: type=integer
+                Allowable error of written value compared to read back value
+            repeats: type=integer
+                Number of times to repeatedly write values
+    """
     # Set custom dependencies for the attr being tested
     for p in depends.keys():
         if isinstance(depends[p], str):
@@ -68,23 +155,51 @@ def attribute_multipe_values_with_depends(
                 assert dev_interface(uri, classname, val, attr, tol) <= tol
 
 
-def attribute_write_only_str(uri, classname, attr, file):
-    # bi = BoardInterface(classname, devicename)
+def attribute_write_only_str(uri, classname, attr, value):
+    """ attribute_write_only_str: Write only string class
+            property
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            value: type=string
+                Value to write into attr property
+    """
     sdr = eval(classname + "(uri='" + uri + "')")
     try:
-        setattr(sdr, attr, file)
+        setattr(sdr, attr, value)
         del sdr
     except Exception as e:
         del sdr
         raise Exception(e)
 
 
-def attribute_write_only_str_with_depends(uri, classname, attr, profile_file, depends):
+def attribute_write_only_str_with_depends(uri, classname, attr, value, depends):
+    """ attribute_write_only_str_with_depends: Write only string class
+            property with dependent write only properties
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            attr: type=string
+                Attribute name to be written. Must be property of classname
+            value: type=string
+                Value to write into attr property
+            depends: type=dict
+                Dictionary of properties to write before value is written. Keys
+                are properties and values are values to be written
+    """
     sdr = eval(classname + "(uri='" + uri + "')")
     for p in depends:
         setattr(sdr, p, depends[p])
     try:
-        setattr(sdr, attr, profile_file)
+        setattr(sdr, attr, value)
         del sdr
     except Exception as e:
         del sdr
@@ -92,13 +207,21 @@ def attribute_write_only_str_with_depends(uri, classname, attr, profile_file, de
 
 
 def dma_rx(uri, classname, channel):
-    # bi = BoardInterface(classname, devicename)
+    """ dma_rx: Construct RX buffers and verify data is non-zero when pulled.
+        Collected buffer is of size 2**15 and 10 buffers are checked
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            channel: type=list
+                List of integers or list of list of integers of channels to
+                enable through rx_enabled_channels
+    """
     sdr = eval(classname + "(uri='" + uri + "')")
     N = 2 ** 15
-    if not isinstance(channel, list):
-        sdr.rx_enabled_channels = [channel]
-    else:
-        sdr.rx_enabled_channels = channel
+    sdr.rx_enabled_channels = channel if isinstance(channel, list) else [channel]
     sdr.rx_buffer_size = N * len(sdr.rx_enabled_channels)
     try:
         for _ in range(10):
@@ -116,7 +239,18 @@ def dma_rx(uri, classname, channel):
 
 
 def dma_tx(uri, classname, channel):
-    # bi = BoardInterface(classname, devicename)
+    """ dma_tx: Construct TX buffers and verify no errors occur when pushed.
+        Buffer is of size 2**15 and 10 buffers are pushed
+
+        parameters:
+            uri: type=string
+                URI of IIO context of target board/system
+            classname: type=string
+                Name of pyadi interface class which contain attribute
+            channel: type=list
+                List of integers or list of list of integers of channels to
+                enable through tx_enabled_channels
+    """
     sdr = eval(classname + "(uri='" + uri + "')")
     TXFS = 1000
     N = 2 ** 15
