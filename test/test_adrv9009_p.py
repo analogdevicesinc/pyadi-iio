523f3e65d6a5653dd555b06261a0f41e1198753e
Travis F. Collins <travis.collins@analog.com>
2020-11-23 15:11:36 -0800

    Add fs.sshfs import explicitly to JESD class
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_adrv9009_p.py
+++ test/test_adrv9009_p.py
@@ -1,6 +1,8 @@
 import pytest
+
 try:
     import adi.jesd
+
     skip_jesd = False
 except:
     skip_jesd = True
@@ -75,9 +77,7 @@ def test_adrv9009_rx_data(test_dma_rx, iio_uri, classname, channel):
     ],
 )
 @pytest.mark.parametrize("sfdr_min", [45])
-def test_adrv9009_sfdr(
-    test_sfdr, iio_uri, classname, channel, param_set, sfdr_min
-):
+def test_adrv9009_sfdr(test_sfdr, iio_uri, classname, channel, param_set, sfdr_min):
     test_sfdr(iio_uri, classname, channel, param_set, sfdr_min)
 
 
@@ -227,9 +227,7 @@ def test_adrv9009_dds_gain_check_vary_power(
         ),
     ],
 )
-def test_adrv9009_iq_loopback(
-    test_iq_loopback, iio_uri, classname, channel, param_set
-):
+def test_adrv9009_iq_loopback(test_iq_loopback, iio_uri, classname, channel, param_set):
     test_iq_loopback(iio_uri, classname, channel, param_set)
 
 
@@ -238,7 +236,7 @@ def test_adrv9009_iq_loopback(
 @pytest.mark.iio_hardware(hardware)
 def test_adrv9009_jesd(iio_uri):
     import adi
+
     sdr = adi.adrv9009(uri=iio_uri, jesd_monitor=True)
     info = sdr._jesd.get_all_statuses()
     assert info
-
