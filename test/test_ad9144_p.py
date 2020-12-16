6629d8750c6a0624b1955c3a952175e08b55291b
Trecia Agoylo <trecia.agoylo@analog.com>
2020-12-16 16:34:49 +0800

    Update arguments on ad9144 test
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- test/test_ad9144_p.py
+++ test/test_ad9144_p.py
@@ -8,4 +8,4 @@ classname = "adi.ad9144"
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize("channel", [0, 1, [0, 1]])
 def test_ad9144_tx_data(test_dma_tx, iio_uri, classname, channel):
-    test_dma_tx(iio_uri, classname, hardware, channel)
+    test_dma_tx(iio_uri, classname, channel)
