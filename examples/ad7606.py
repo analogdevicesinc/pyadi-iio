3239e01c750711af32bb2a91f552663cd0d6e4fd
mahphalke <Mahesh.Phalke@analog.com>
2021-03-02 18:17:30 +0530

    Added pyadi-iio support files for AD7606 devices
    Signed-off-by: mahphalke <Mahesh.Phalke@analog.com>

--- examples/ad7606.py
+++ examples/ad7606.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Analog Devices, Inc.
+# Copyright (C) 2021 Analog Devices, Inc.
 #
 # All rights reserved.
 #
@@ -31,68 +31,23 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from adi.ad936x import ad9361, ad9363, ad9364, Pluto
+import adi
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
 
-from adi.fmcomms5 import FMComms5
+# Set up AD7606
+ad7606 = adi.ad7606(uri="ip:analog")
+ad_channel = 0
 
-from adi.ad9371 import ad9371
+sc = ad7606.scale_available
 
-from adi.adrv9002 import adrv9002
+ad7606.channel[ad_channel].scale = sc[-1]  # get highest range
 
-from adi.adrv9009 import adrv9009
+ad7606.rx_enabled_channels = [ad_channel]
+ad7606.rx_buffer_size = 100
 
-from adi.adrv9009_zu11eg import adrv9009_zu11eg
+raw = ad7606.channel[0].raw
+data = ad7606.rx()
 
-from adi.adrv9009_zu11eg_multi import adrv9009_zu11eg_multi
-
-from adi.adrv9009_zu11eg_fmcomms8 import adrv9009_zu11eg_fmcomms8
-
-from adi.ad9081 import ad9081
-
-from adi.ad9081_mc import ad9081_mc, QuadMxFE
-
-from adi.ad9094 import ad9094
-
-from adi.ad9680 import ad9680
-
-from adi.ad9144 import ad9144
-
-from adi.ad9152 import ad9152
-
-from adi.cn0532 import cn0532
-
-from adi.daq2 import DAQ2
-
-from adi.daq3 import DAQ3
-
-from adi.adis16460 import adis16460
-
-from adi.adis16507 import adis16507
-
-from adi.ad7124 import ad7124
-
-from adi.adxl345 import adxl345
-
-from adi.adxrs290 import adxrs290
-
-from adi.fmclidar1 import fmclidar1
-
-from adi.ad5686 import ad5686
-
-from adi.adar1000 import adar1000, adar1000_array
-
-from adi.ltc2983 import ltc2983
-
-from adi.one_bit_adc_dac import one_bit_adc_dac
-
-from adi.ltc2314_14 import ltc2314_14
-
-from adi.ad7606 import ad7606
-
-try:
-    from adi.jesd import jesd
-except ImportError:
-    pass
-
-__version__ = "0.0.8"
-name = "Analog Devices Hardware Interfaces"
+print(data)
