c62a78e6aa20cf8ba3848a6b58754c7861244ee6
Michael Hennerich <michael.hennerich@analog.com>
2021-08-04 15:44:30 +0200

    examples/ad9083_vna.py: Update test/example
     * Add ADL5960_SYNCx controls
     * use ADL5960 if_frequency
     * Test clock doubler support
     * Add some debug prints
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- examples/ad9083_vna.py
+++ examples/ad9083_vna.py
@@ -97,6 +97,11 @@ vna = adi.fmcvna("ip:analog.local")
 # Configure properties
 print("--Setting up chip")
 
+#Capture all 32 channels
+vna.rx_enabled_channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+vna.rx_buffer_size = 2 ** 12
+fs = int(vna.rx_sample_rate)
+
 # ADRF5720
 vna.lo_attenuator.attenuation = 6
 vna.rfin_attenuator.attenuation = 6
@@ -105,34 +110,46 @@ vna.lo_mux.select = 'bypass'
 vna.rfin_mux.select = 'd1'
 vna.lo.frequency = 3e9
 
-offset_frequency = 3906250
+# push shifted DC the out of spectrum
+if_frequency = fs
 
-vna.nco0_frequency = offset_frequency
+vna.nco0_frequency = if_frequency
 
 for i in range (0, 8):
     vna.frontend[i].lo_mode = 'x1'
     vna.frontend[i].offset_mode = '/4'
-    vna.frontend[i].offset_frequency = offset_frequency
+    vna.frontend[i].if_frequency = if_frequency
     vna.frontend[i].forward_gain = 6
     vna.frontend[i].reflected_gain = 6
-    vna.frontend[i].if_filter_cutoff = vna.frontend[i].offset_frequency
+    vna.frontend[i].if_filter_cutoff = vna.frontend[i].if_frequency
     print ("ADL5960-", i, "Temperature", vna.frontend[i].temperature, "°C")
     print ("ADL5960-", i, "REG 0x25 =", vna.frontend[i].reg_read(0x25))
 
-#Capture all 32 channels
-vna.rx_enabled_channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-vna.rx_buffer_size = 2 ** 12
-fs = int(vna.rx_sample_rate)
 
-print("NCO0", vna.nco0_frequency, "Hz")
+vna.gpio_adl5960x_sync = 1
+vna.gpio_adl5960x_sync = 0
+
+print("AD9083    NCO0", vna.nco0_frequency, "Hz")
+print("ADL5960-1 IF frequency", vna.frontend[0].if_frequency, "Hz")
+print("ADL5960-1 OFFSET frequency", vna.frontend[0].offset_frequency, "Hz")
+print("ADL5960-1 OFFSET mode", vna.frontend[0].offset_mode)
+
+div = 1
 
 # Collect data
-for f in range (int(3e9), int(8e9), int(100e6)):
-    vna.lo.frequency = f
+for f in range (int(3e9), int(18e9), int(1000e6)):
+    if f > 7e9:
+        for i in range (0, 8):
+            vna.frontend[i].lo_mode = 'x2'
+        vna.lo_mux.select = 'doubler'
+        div = 0.5
+
+    vna.lo.frequency = f * div
+    print ("ADL5960-", i, "CT2 REG 0x21 =", vna.frontend[0].reg_read(0x21))
     # ADMV8818 should update automatically as long as the LO doubler is not used
     print ("LO   HPF", vna.lo_bpf.high_pass_3db_frequency, "MHz, LPF", vna.lo_bpf.low_pass_3db_frequency, "MHz")
     print ("RFIN HPF", vna.rfin_bpf.high_pass_3db_frequency, "MHz, LPF", vna.rfin_bpf.low_pass_3db_frequency, "MHz")
-    for r in range(16):
+    for r in range(2):
         i = int(r / 2) + 1
         vna.rfin_mux.select = f"d{i}"
         x = vna.rx()
