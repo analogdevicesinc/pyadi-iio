3e716e6f06b420fe1c32cb3b2899176089dd1b21
Travis F. Collins <travis.collins@analog.com>
2021-10-12 16:26:51 -0700

    Add DMA example for AD9081
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ examples/ad9081_dma_example.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2021 Analog Devices, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import adi
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+
+
+def gen_tone(fc, fs, N):
+    fc = int(fc / (fs / N)) * (fs / N)
+    ts = 1 / float(fs)
+    t = np.arange(0, N * ts, ts)
+    i = np.cos(2 * np.pi * t * fc) * 2 ** 14
+    q = np.sin(2 * np.pi * t * fc) * 2 ** 14
+    return i + 1j * q
+
+
+dev = adi.ad9081("ip:analog-2.local")
+
+# Configure properties
+print("--Setting up chip")
+
+# Set NCOs
+dev.rx_channel_nco_frequencies = [0] * 4
+dev.tx_channel_nco_frequencies = [0] * 4
+
+dev.rx_main_nco_frequencies = [1000000000] * 4
+dev.tx_main_nco_frequencies = [1000000000] * 4
+
+dev.rx_enabled_channels = [0]
+dev.tx_enabled_channels = [0]
+dev.rx_nyquist_zone = "odd"
+
+dev.rx_buffer_size = 2 ** 16
+dev.tx_cyclic_buffer = True
+
+fs = int(dev.tx_sample_rate)
+
+# Create a sinewave waveform
+fs = int(dev.rx_sample_rate)
+N = 1024
+iq1 = gen_tone(-60e6, fs, N)
+iq2 = gen_tone(100e6, fs, N)
+
+
+# Enable BRAM offload in FPGA (Necessary for high rates)
+dev.tx_ddr_offload = 1
+
+# Send data
+dev.tx(iq1)
+
+# Collect data
+for r in range(40):
+    x = dev.rx()
+
+    if r == 20:
+        dev.tx_destroy_buffer()
+        dev.tx(iq2)
+
+    f, Pxx_den = signal.periodogram(x, fs, return_onesided=False)
+    plt.clf()
+    plt.semilogy(f, Pxx_den)
+    plt.ylim([1e-8, 1e5])
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("PSD")
+    plt.draw()
+    plt.pause(0.05)
+
+plt.show()
