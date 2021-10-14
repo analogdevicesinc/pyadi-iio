cc7a6c0db6816ee095fa00dbef1634adcd7586b4
Travis F. Collins <travis.collins@analog.com>
2021-10-13 17:30:54 -0700

    Force pylibiio 0.23.1 to resolve yanked version
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     url="https://github.com/analogdevicesinc/pyadi-iio",
     packages=setuptools.find_packages(exclude=["test*"]),
     python_requires=">=3.6",
-    install_requires=["numpy", "pylibiio==0.23", "paramiko"],
+    install_requires=["numpy", "pylibiio==0.23.1", "paramiko"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
