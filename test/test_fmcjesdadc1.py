82e1df5f54e85f2c791a2b972f60f50ab416fafb
Hannah Rosete <Hannah.Rosete@analog.com>
2021-10-21 14:55:56 +0800

    Add support for FMCJESDADC1
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

---
+++ test/test_fmcjesdadc1.py
@@ -0,0 +1,12 @@
+import pytest
+
+hardware = ["fmcjesdadc1"]
+classname = ["adi.fmcjesdadc1"]
+
+
+#########################################
+@pytest.mark.iio_hardware(hardware, True)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0, 1, [0, 1]])
+def test_fmcjesdadc1_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel)
