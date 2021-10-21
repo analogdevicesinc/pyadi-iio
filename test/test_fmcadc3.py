6ef67aef8b2f8adfce0f37c9dc7083544d8b0399
Hannah Rosete <Hannah.Rosete@analog.com>
2021-10-21 14:55:28 +0800

    Add support for FMCADC3
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

---
+++ test/test_fmcadc3.py
@@ -0,0 +1,12 @@
+import pytest
+
+hardware = ["fmcadc3"]
+classname = ["adi.fmcadc3"]
+
+
+#########################################
+@pytest.mark.iio_hardware(hardware, True)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0])
+def test_fmcadc3_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel)
