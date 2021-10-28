4c6fed477db3c048efb78e01e6782154bf421d47
Travis F. Collins <travis.collins@analog.com>
2021-10-28 14:41:34 -0700

    Add markdown support for doc
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,12 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages", "sphinx_rtd_theme"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
+    "sphinx_rtd_theme",
+    "myst_parser",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
