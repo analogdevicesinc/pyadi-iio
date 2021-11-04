44e0b851a4fb31a7f212a9b392cfb8a9ad131cab
Darshil Trivedi <darshiltrivedi04@gmail.com>
2021-11-03 17:22:17 -0700

    pre-commit check passed

--- adi/adf4159.py
+++ adi/adf4159.py
@@ -60,19 +60,23 @@ class adf4159(context_manager, attribute):
     def ramp_mode(self, value):
         """Get/Set the Ramp output mode"""
 
-        valid = self._get_iio_attr_str("altvoltage0", "ramp_mode_available", True, self._ctrl)
+        valid = self._get_iio_attr_str(
+            "altvoltage0", "ramp_mode_available", True, self._ctrl
+        )
         if value not in valid:
 
             raise ValueError(
-                f"ramp_mode of \"{value}\" is invalid. Valid options: \"{valid}\""
+                f'ramp_mode of "{value}" is invalid. Valid options: "{valid}"'
             )
 
-        self._set_iio_attr("altvoltage0", "ramp_mode", True,  value, self._ctrl)
+        self._set_iio_attr("altvoltage0", "ramp_mode", True, value, self._ctrl)
 
     @property
     def enable(self):
         """Get/Set the enable status of the RF output"""
-        return bool(int(self._get_iio_attr("altvoltage0", "powerdown", True, self._ctrl)))
+        return bool(
+            int(self._get_iio_attr("altvoltage0", "powerdown", True, self._ctrl))
+        )
 
     @enable.setter
     def enable(self, value):
@@ -92,29 +96,41 @@ class adf4159(context_manager, attribute):
     @property
     def freq_dev_range(self):
         """Get/Set the PLL frequency deviation range"""
-        return self._get_iio_attr("altvoltage0", "frequency_deviation_range", True, self._ctrl)
+        return self._get_iio_attr(
+            "altvoltage0", "frequency_deviation_range", True, self._ctrl
+        )
 
     @freq_dev_range.setter
     def freq_dev_range(self, value):
         """Get/Set the PLL frequency deviation range"""
-        self._set_iio_attr("altvoltage0", "frequency_deviation_range", True, value, self._ctrl)
+        self._set_iio_attr(
+            "altvoltage0", "frequency_deviation_range", True, value, self._ctrl
+        )
 
     @property
     def freq_dev_step(self):
         """Get/Set the PLL frequency deviation step"""
-        return self._get_iio_attr("altvoltage0", "frequency_deviation_step", True, self._ctrl)
+        return self._get_iio_attr(
+            "altvoltage0", "frequency_deviation_step", True, self._ctrl
+        )
 
     @freq_dev_step.setter
     def freq_dev_step(self, value):
         """Get/Set the PLL frequency deviation step"""
-        self._set_iio_attr("altvoltage0", "frequency_deviation_step", True,  (1 + value), self._ctrl)
+        self._set_iio_attr(
+            "altvoltage0", "frequency_deviation_step", True, (1 + value), self._ctrl
+        )
 
     @property
     def freq_dev_time(self):
         """Get/Set the PLL frequency deviation time"""
-        return self._get_iio_attr("altvoltage0", "frequency_deviation_time", True, self._ctrl)
+        return self._get_iio_attr(
+            "altvoltage0", "frequency_deviation_time", True, self._ctrl
+        )
 
     @freq_dev_time.setter
     def freq_dev_time(self, value):
         """Get/Set the PLL frequency deviation time"""
-        self._set_iio_attr("altvoltage0", "frequency_deviation_time", True, value, self._ctrl)
+        self._set_iio_attr(
+            "altvoltage0", "frequency_deviation_time", True, value, self._ctrl
+        )
