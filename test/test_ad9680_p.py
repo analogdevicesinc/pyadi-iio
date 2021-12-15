a9871451828b361ac7a2b23a0d0ed6d29e5a979c
Travis F. Collins <travis.collins@analog.com>
2021-12-15 08:40:49 -0800

    Update tests for newly emulated hardware
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_ad9680_p.py
+++ test/test_ad9680_p.py
@@ -4,7 +4,7 @@ hardware = "daq2"
 classname = "adi.ad9680"
 
 
-@pytest.mark.iio_hardware(hardware)
+@pytest.mark.iio_hardware(hardware, True)
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize("channel", [0, 1, [0, 1]])
 def test_ad9680_rx_data(test_dma_rx, iio_uri, classname, channel):
