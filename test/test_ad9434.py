207cc40bb55a5f4907fe95ae572aa34704ec5f46
Travis F. Collins <travis.collins@analog.com>
2022-03-01 08:53:36 -0800

    Fix AD9434 class and add tests
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_ad9434.py
+++ test/test_ad9434.py
@@ -1,7 +1,7 @@
 import pytest
 
-hardware = ["ad9434"]
-classname = ["adi.ad9434"]
+hardware = "ad9434"
+classname = "adi.ad9434"
 
 
 #########################################
@@ -10,3 +10,27 @@ classname = ["adi.ad9434"]
 @pytest.mark.parametrize("channel", [0])
 def test_ad9434_rx_data(test_dma_rx, iio_uri, classname, channel):
     test_dma_rx(iio_uri, classname, channel)
+
+
+#########################################
+@pytest.mark.iio_hardware(hardware)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("attr", ["test_mode"])
+@pytest.mark.parametrize(
+    "val",
+    [
+        "off",
+        "midscale_short",
+        "pos_fullscale",
+        "neg_fullscale",
+        "checkerboard",
+        "pn_long",
+        "pn_short",
+        "one_zero_toggle",
+        "user",
+    ],
+)
+def test_ad9434_loopback_attr(
+    test_attribute_single_value_str, iio_uri, classname, attr, val
+):
+    test_attribute_single_value_str(iio_uri, classname, attr, val, 0)
