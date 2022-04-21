455317f9c2166acf57f35061233e5f1c875dee01
VCalinao <victor.calinaojr@analog.com>
2022-04-21 14:24:19 +0800

    ad5592r: added initial support

--- examples/ad5592r.py
+++ examples/ad5592r.py
@@ -6,6 +6,6 @@ myad5592r = adi.ad5592r(uri="ip:analog.local")
 # Read channels and its parameters
 print(myad5592r.channel)
 for ch in myad5592r.channel:
-    print(ch.name)
-    print(ch._output)
-    print(ch.raw)
+    print("Channel Name: " + ch.name)
+    print("Output Channel: " + ch._output)
+    print("Channel Raw Value: " + ch.raw)
