359c96219cf1a4d441320e2175a56f0c369138a4
Ciprian Regus <ciprian.regus@analog.com>
2022-06-24 11:16:28 +0300

    Added a dma_rx test for the max31855
    Added a basic dma_rx channel read test. Only this one was added
    because the device doesn't have any writtable attributes.

---
+++ test/test_max31855.py
@@ -0,0 +1,12 @@
+import adi
+import pytest
+
+hardware = ["MAX31855"]
+classname = "adi.max31855"
+
+#########################################
+@pytest.mark.iio_hardware(hardware)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0, 1])
+def test_max31855_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel, buffer_size=32)
