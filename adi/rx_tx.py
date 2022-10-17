97fb1f213cad4df8f36737b3d7c079698d02869e
Adrian Suciu <adrian.suciu@analog.com>
2022-10-17 13:12:45 +0300

    rx_tx: convert from channel to sample interleaved by default
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- adi/rx_tx.py
+++ adi/rx_tx.py
@@ -98,7 +98,7 @@ class rx(rx_tx_common):
     __rxbuf = None
     _rx_unbuffered_data = False
     _rx_annotated = False
-    _rx_stack_interleaved = False  # Convert from channel to sample interleaved
+    _rx_stack_interleaved = True  # Convert from channel to sample interleaved
 
     def __init__(self, rx_buffer_size=1024):
         if self._complex_data:
@@ -301,7 +301,9 @@ class rx(rx_tx_common):
         x = data
 
         stride = len(self.rx_enabled_channels)
-        if self._rx_stack_interleaved:
+
+        # only makes sense when multiple channels are enabled
+        if self._rx_stack_interleaved and stride > 1:
             # Convert data to sample interleaved from channel interleaved
             sigi = np.empty((x.size,), dtype=x.dtype)
             for i, _ in enumerate(self.rx_enabled_channels):
