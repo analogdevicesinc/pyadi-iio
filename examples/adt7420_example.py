21a31fe2d646928173dd188305598cbe1da3793b
Jose Ramon San Buenaventura <jose.sanbuenaventura@analog.com>
2022-11-09 01:44:16 +0800

    Formatting changes
    Signed-off-by: Jose Ramon San Buenaventura <jose.sanbuenaventura@analog.com>

--- examples/adt7420_example.py
+++ examples/adt7420_example.py
@@ -31,17 +31,20 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import adi
 import time
+
+import adi
 
 temp_sensor = adi.adt7420(uri="serial:COM24,57600,8N1")
 
-print('Max Temperature Threshold = ' + str(temp_sensor.temp.temp_max))
-print('Min Temperature Threshold = ' + str(temp_sensor.temp.temp_min))
-print('Crit Temperature Threshold = ' + str(temp_sensor.temp.temp_crit))
-print('Hysteresis Temperature Setting = ' + str(temp_sensor.temp.temp_hyst))
+print("Max Temperature Threshold = " + str(temp_sensor.temp.temp_max))
+print("Min Temperature Threshold = " + str(temp_sensor.temp.temp_min))
+print("Crit Temperature Threshold = " + str(temp_sensor.temp.temp_crit))
+print("Hysteresis Temperature Setting = " + str(temp_sensor.temp.temp_hyst))
 
 datapoints = 300
 for i in range(datapoints):
-    print('Temperature Measurement ' + str(i + 1) + ' = ' + str(temp_sensor.temp.temp_val))
+    print(
+        "Temperature Measurement " + str(i + 1) + " = " + str(temp_sensor.temp.temp_val)
+    )
     time.sleep(0.1)
