Fix dma_rx test overflow issue. dma_rx passes as long as there's at least one nonzero data point. Using np.sum(np.max(data)) can overflow to a negative value, particularly with high-resolution ADCs and long buffers, failing the test. Replace with np.max(np.abs(data))
Signed-off-by: Mark Thoren <mark.thoren@analog.com>

test/dma_tests.py
@@ -61,9 +61,9 @@ def dma_rx(
             else:
                 if isinstance(data, list):
                     for chan in data:
-                        assert np.sum(np.abs(chan)) > 0, "Buffer all zeros"
+                        assert np.max(np.abs(chan)) > 0, "Buffer all zeros"
                 else:
-                    assert np.sum(np.abs(data)) > 0, "Buffer all zeros"
+                    assert np.max(np.abs(data)) > 0, "Buffer all zeros"
     except Exception as e:
         del sdr
         raise Exception(e) from e