4498576e9ee31ab4a2926b992eb4dffe15c374fd
Acut <Nathaniel.Acut@analog.com>
2023-03-17 16:38:36 +0800

    Adding support for MAX9611
    Signed-off-by: Nathaniel Acut <nathaniel.acut@analog.com>

---
+++ test/test_max9611.py
@@ -0,0 +1,12 @@
+import adi
+import pytest
+
+hardware = ["max9611"]
+classname = "adi.max9611"
+
+#########################################
+@pytest.mark.iio_hardware(hardware)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0, 1])
+def test_max9611_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel, buffer_size=32)
