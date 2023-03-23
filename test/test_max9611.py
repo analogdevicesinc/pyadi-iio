df8b7858c18f8d2b636f7e65b2c90d42e857a873
Nathaniel Acut <nathaniel.acut@analog.com>
2023-03-23 17:21:08 +0800

    Modified max9611 test
    Signed-off-by: Nathaniel Acut <nathaniel.acut@analog.com>

--- test/test_max9611.py
+++ test/test_max9611.py
@@ -7,6 +7,25 @@ classname = "adi.max9611"
 #########################################
 @pytest.mark.iio_hardware(hardware)
 @pytest.mark.parametrize("classname", [(classname)])
-@pytest.mark.parametrize("channel", [0])
-def test_max9611_rx_data(test_dma_rx, iio_uri, classname, channel):
-    test_dma_rx(iio_uri, classname, channel, buffer_size=32)
+@pytest.mark.parametrize(
+    "attr, start, stop, step, tol, repeats, sub_channel",
+    [
+        ("raw", 0, 1023, 1, 1, 3, "_channel_voltage_input"),
+        ("raw", 0, 1023, 1, 1, 3, "_channel_temp"),
+    ],
+)
+def test_max9611_raw_attr(
+    test_attribute_single_value,
+    iio_uri,
+    classname,
+    attr,
+    start,
+    stop,
+    step,
+    tol,
+    repeats,
+    sub_channel,
+):
+    test_attribute_single_value(
+        iio_uri, classname, attr, start, stop, step, tol, repeats, sub_channel
+    )
