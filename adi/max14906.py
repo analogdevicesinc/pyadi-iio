a91380dc60ea6325bc02c83dbf3a4a19d6ec88c5
Ciprian Regus <ciprian.regus@analog.com>
2023-04-26 16:44:51 +0300

    adi: max14906: Add support for MAX14906
    Implement support for the MAX14906.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

---
+++ adi/max14906.py
@@ -0,0 +1,52 @@
+from collections import OrderedDict
+
+import numpy as np
+from adi.attribute import attribute
+from adi.context_manager import context_manager
+from adi.rx_tx import rx
+
+class max14906(rx, context_manager):
+
+        _device_name = "max14906"
+
+        def __init__(self, uri=""):
+                context_manager.__init__(self, uri, self._device_name)
+                self._ctrl = self._ctx.find_device("max14906")
+
+                if not self._ctrl:
+                        raise Exception("No max14906 device found")
+
+                _channels = []
+                for ch in self._ctrl.channels:
+                        self._rx_channel_names.append(ch.id)
+                        _channels.append((ch.id, self._channel(self._ctrl, ch.id)))
+                self.channel = OrderedDict(_channels)
+
+        def reg_read(self, reg):
+                self._set_iio_debug_attr_str("direct_reg_access", reg, self._ctrl)
+                return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
+
+        def reg_write(self, reg, value):
+                self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)
+
+        class _channel(attribute):
+
+                def __init__(self, ctrl, channel_name):
+                        self.name = channel_name
+                        self._ctrl = ctrl
+
+                @property
+                def raw(self):
+                        return self._get_iio_attr(self.name, "raw")
+
+                @raw.setter
+                def raw(self, value):
+                        return self._set_iio_attr(self.name, "raw", value)
+
+                @property
+                def scale(self):
+                        return self._get_iio_attr(self.name, "scale")
+
+                @property
+                def offset(self):
+                        return self._get_iio_attr(self.name, "offset")
