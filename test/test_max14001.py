16a8557c9d979ad423d7b185f1b44cd6ddfe6266
ivangilmercano <128690159+ivangilmercano@users.noreply.github.com>
2023-05-16 00:00:19 +0800

    add new support for max14001 adc (#429)
    new commit for new pr max14001
    fix for comments in  max14001 pyadi-iio
    
    Signed-off-by: Ivan Gil Mercano <ivangil.mercano@analog.com>

---
+++ test/test_max14001.py
@@ -0,0 +1,28 @@
+import pytest
+
+hardware = "max14001"
+classname = "adi.max14001"
+
+
+######################################
+@pytest.mark.iio_hardware(hardware)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize(
+    "attr, start, stop, step, tol, repeats, sub_channel",
+    [("raw", 0, 1023, 1, 1, 2, "_channel"),],
+)
+def test_max14001_attr(
+    test_attribute_single_value,
+    iio_uri,
+    classname,
+    attr,
+    start,
+    stop,
+    step,
+    tol,
+    repeats,
+    sub_channel,
+):
+    test_attribute_single_value(
+        iio_uri, classname, attr, start, stop, step, tol, repeats, sub_channel
+    )
