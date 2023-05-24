7d0131785a0d7fada1ad965824d2fab5c2e0b663
Ciprian Regus <ciprian.regus@analog.com>
2023-05-24 17:08:00 +0300

    add example for swiot

--- adi/ad74413r.py
+++ adi/ad74413r.py
@@ -7,60 +7,69 @@ from adi.rx_tx import rx
 
 class ad74413r(rx, context_manager):
 
-        _device_name = "ad74413r"
+	_device_name = "ad74413r"
 
-        def __init__(self, uri=""):
-                context_manager.__init__(self, uri, self._device_name)
-                self._ctrl = self._ctx.find_device("ad74413r")
+	def __init__(self, uri=""):
+		context_manager.__init__(self, uri, self._device_name)
+		self._ctrl = self._ctx.find_device("ad74413r")
+		self._rxadc = self._ctx.find_device("ad74413r")
+		self._trigger = self._ctx.find_device("ad74413r-dev0")
+		self._rxadc._set_trigger(self._trigger)
+		self._rx_channel_names = []
+		self._tx_channel_names = []
 
-                if not self._ctrl:
-                        raise Exception("No ad74413r device found")
+		if not self._ctrl:
+			raise Exception("No ad74413r device found")
 
-                _channels = []
-                for ch in self._ctrl.channels:
-                        self._rx_channel_names.append(ch.id)
-                        _channels.append((ch.id, self._channel(self._ctrl, ch.id)))
-                self.channel = OrderedDict(_channels)
+		_channels = []
+		for ch in self._ctrl.channels:
+			if ch.output:
+				self._tx_channel_names.append(ch.id)
+			else:
+				self._rx_channel_names.append(ch.id)
+			_channels.append((ch.id, self._channel(self._ctrl, ch.id, ch.output)))
+		self.channel = OrderedDict(_channels)
 
-                rx.__init__(self)
+		rx.__init__(self)
 
-        def reg_read(self, reg):
-                self._set_iio_debug_attr_str("direct_reg_access", reg, self._ctrl)
-                return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
+	def reg_read(self, reg):
+		self._set_iio_debug_attr_str("direct_reg_access", reg, self._ctrl)
+		return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
 
-        def reg_write(self, reg, value):
-                self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)
+	def reg_write(self, reg, value):
+		self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)
 
-        class _channel(attribute):
+	class _channel(attribute):
 
-                def __init__(self, ctrl, channel_name):
-                        self.name = channel_name
-                        self._ctrl = ctrl
+		def __init__(self, ctrl, channel_name, output):
+			self.name = channel_name
+			self._ctrl = ctrl
+			self.output = output
 
-                @property
-                def raw(self):
-                        return self._get_iio_attr(self.name, "raw")
+		@property
+		def raw(self):
+			return self._get_iio_attr(self.name, "raw", self.output)
 
-                @raw.setter
-                def raw(self, value):
-                        return self._set_iio_attr(self.name, "raw", value)
+		@raw.setter
+		def raw(self, value):
+			return self._set_iio_attr(self.name, "raw", self.output, value)
 
-                @property
-                def scale(self):
-                        return self._get_iio_attr(self.name, "scale")
+		@property
+		def scale(self):
+			return self._get_iio_attr(self.name, "scale", self.output)
 
-                @property
-                def offset(self):
-                        return self._get_iio_attr(self.name, "offset")
+		@property
+		def offset(self):
+			return self._get_iio_attr(self.name, "offset", self.output)
 
-                @property
-                def sampling_frequency(self):
-                        return self._get_iio_attr(self.name, "sampling_frequency")
+		@property
+		def sampling_frequency(self):
+			return self._get_iio_attr(self.name, "sampling_frequency", self.output)
 
-                @sampling_frequency.setter
-                def sampling_frequency(self, value):
-                        return self._set_iio_attr(self.name, "sampling_frequency", value)
+		@sampling_frequency.setter
+		def sampling_frequency(self, value):
+			return self._set_iio_attr(self.name, "sampling_frequency", self.output, value)
 
-                @property
-                def processed(self):
-                        return self._get_iiio_attr(self.name, "processed")
+		@property
+		def processed(self):
+			return self._get_iiio_attr(self.name, "processed", self.output)
