01ed9cfd72639751ffbcc2e2ff545caa24585458
Ciprian Regus <ciprian.regus@analog.com>
2023-06-08 14:31:29 +0300

    update the swiot example

--- examples/swiot.py
+++ examples/swiot.py
@@ -1,9 +1,48 @@
 import adi
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 dev_uri = "ip:swiot.local"
 
+"""
+	Possible values:
+	- max14906 selected as device: input, output, high_z
+	- ad74413r selected as device: high_z, voltage_out, current_out,
+				       voltage_in, current_in_ext, current_in_loop,
+				       resistance, digital_input, digital_input_loop,
+				       current_in_ext_hart, current_in_loop_hart
+
+"""
+channel_config = ["voltage_in", "resistance", "input", "output"]
+
+# Possible values: 0, 1
+channel_enable = [1, 1, 1, 1]
+
+# Possible values: "ad74413r", "max14906"
+channel_device = ["ad74413r", "ad74413r", "max14906", "max14906"]
+
+swiot = adi.swiot(uri=dev_uri)
+swiot.mode = "config"
+swiot = adi.swiot(uri=dev_uri)
+
+swiot.ch0_device = channel_device[0]
+swiot.ch0_function = channel_config[0]
+swiot.ch0_enable = channel_enable[0]
+swiot.ch1_device = channel_device[1]
+swiot.ch1_function = channel_config[1]
+swiot.ch1_enable = channel_enable[1]
+swiot.ch2_device = channel_device[2]
+swiot.ch2_function = channel_config[2]
+swiot.ch2_enable = channel_enable[2]
+swiot.ch3_device = channel_device[3]
+swiot.ch3_function = channel_config[3]
+swiot.ch3_enable = channel_enable[3]
+swiot.mode = "runtime"
+
+time.sleep(3)
+
+swiot = adi.swiot(uri=dev_uri)
 ad74413r = adi.ad74413r(uri=dev_uri)
 max14906 = adi.max14906(uri=dev_uri)
 adt75 = adi.lm75(uri=dev_uri)
@@ -26,7 +65,7 @@ print("MAX14906 input channels:", max14906._rx_channel_names)
 print("MAX14906 output channels:", max14906._tx_channel_names)
 
 # Setting the output value of MAX14906 channel (0 or 1)
-max14906.channel["voltage3"].raw = 0
+max14906.channel["voltage3"].raw = 1
 
 # Reading the value of a MAX14906 channel. It's the same for either input or output channels.
 print("MAX14906 channel 3 value:", max14906.channel["voltage3"].raw)
@@ -48,14 +87,42 @@ ad74413r.rx_buffer_size = 4800
 
 # Start the data sampling. Values stored in this array are converted in mV.
 data = ad74413r.rx()
+ad74413r.rx_destroy_buffer()
 
 # Plot the data. The Y scale is in mV.
 plt.clf()
-# plt.plot(data)
-# plt.ylabel("mV")
-# plt.show(block=True)
-
-counts, bins = np.histogram(data)
-plt.stairs(counts, bins)
+plt.plot(data)
+plt.ylabel("mV")
 plt.show(block=True)
 
+# Reconfig
+swiot.mode = "config"
+swiot = adi.swiot(uri=dev_uri)
+channel_config[0] = "voltage_in"
+channel_config[3] = "high_z"
+if swiot.mode == "config":
+	swiot.ch0_device = channel_device[0]
+	swiot.ch0_function = channel_config[0]
+	swiot.ch0_enable = channel_enable[0]
+	swiot.ch1_device = channel_device[1]
+	swiot.ch1_function = channel_config[1]
+	swiot.ch1_enable = channel_enable[1]
+	swiot.ch2_device = channel_device[2]
+	swiot.ch2_function = channel_config[2]
+	swiot.ch2_enable = channel_enable[2]
+	swiot.ch3_device = channel_device[3]
+	swiot.ch3_function = channel_config[3]
+	swiot.ch3_enable = channel_enable[3]
+	swiot.mode = "runtime"
+
+	time.sleep(3)
+
+swiot = adi.swiot(uri=dev_uri)
+ad74413r = adi.ad74413r(uri=dev_uri)
+max14906 = adi.max14906(uri=dev_uri)
+adt75 = adi.lm75(uri=dev_uri)
+# max14906.channel["voltage3"].raw = 0
+
+print("AD74413R voltage0 raw:", ad74413r.channel["voltage0"].raw)
+print("AD74413R voltage0 scale:", ad74413r.channel["voltage0"].scale)
+print("AD74413R voltage0 offset:", ad74413r.channel["voltage0"].offset)
