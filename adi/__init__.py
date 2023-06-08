6fb5c380e915cb7fec42f6ceba52f1757270e5ba
Ciprian Regus <ciprian.regus@analog.com>
2023-06-08 14:31:48 +0300

    add swiot device

--- adi/__init__.py
+++ adi/__init__.py
@@ -125,6 +125,7 @@ from adi.max31865 import max31865
 from adi.one_bit_adc_dac import one_bit_adc_dac
 from adi.QuadMxFE_multi import QuadMxFE_multi
 from adi.tdd import tdd
+from adi.swiot import swiot
 
 try:
     from adi.jesd import jesd
