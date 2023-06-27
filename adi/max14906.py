10907c30520e91ea273a96d56a3c0fa8851f2bd3
Ciprian Regus <ciprian.regus@analog.com>
2023-06-27 12:50:27 +0300

    max14906 updates

--- adi/max14906.py
+++ adi/max14906.py
@@ -5,7 +5,7 @@ from adi.attribute import attribute
 from adi.context_manager import context_manager
 from adi.rx_tx import rx
 
-class max14906(context_manager):
+class max14906(rx, context_manager):
 
 	_device_name = "max14906"
 
@@ -28,11 +28,12 @@ class max14906(context_manager):
 		self.channel = OrderedDict(_channels)
 
 	def reg_read(self, reg):
-		self._set_iio_debug_attr_str("direct_reg_access", reg, self._ctrl)
-		return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
+		return self._ctrl.reg_read(reg)
+		# return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
 
 	def reg_write(self, reg, value):
-		self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)
+		return self._ctrl.reg_write(reg, value)
+		# return self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)
 
 	class _channel(attribute):
 
@@ -56,3 +57,57 @@ class max14906(context_manager):
 		@property
 		def offset(self):
 			return self._get_iio_attr(self.name, "offset", self.output)
+
+		@property
+		def current_limit(self):
+			if not self.output:
+				return 0
+			return self._get_iio_attr(self.name, "current_limit", self.output)
+
+		@current_limit.setter
+		def current_limit(self, value):
+			if not self.output:
+				return 0
+			return self._get_iio_attr(self.name, "current_limit", self.output, value)
+
+		@property
+		def current_limit_available(self):
+			if not self.output:
+				return 0
+			return self._get_iio_attr(self.name, "current_limit_available", self.output)
+
+		@property
+		def do_mode(self):
+			if not self.output:
+				return 0
+			return self._get_iio_attr(self.name, "do_mode", self.output)
+
+		@do_mode.setter
+		def do_mode(self, value):
+			if not self.output:
+				return 0
+			return self._set_iio_attr(self.name, "do_mode", self.output, value)
+
+		@property
+		def do_mode_available(self):
+			if not self.output:
+				return 0
+			return self._get_iio_attr(self.name, "do_mode_available", self.output)
+
+		@property
+		def IEC_type(self):
+			if self.output:
+				return 0
+			return self._get_iio_attr(self.name, "IEC_Type", self.output)
+
+		@IEC_type.setter
+		def IEC_type(self, value):
+			if self.output:
+				return 0
+			return self._set_iio_attr(self.name, "IEC_Type", self.output, value)
+
+		@property
+		def IEC_type_available(self):
+			if self.output:
+				return 0
+			return self._get_iio_attr(self.name, "IEC_Type_available", self.output)
