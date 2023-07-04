e085dbfc26e92c5962192c9192336e255470a711
Marcelo Schmitt <marcelo.schmitt@analog.com>
2023-07-04 17:19:06 -0300

    Apply Codacy suggestion for adi/mwpicore.py

--- adi/mwpicore.py
+++ adi/mwpicore.py
@@ -34,7 +34,6 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import numpy as np
 from adi.attribute import attribute
 from adi.context_manager import context_manager
 
