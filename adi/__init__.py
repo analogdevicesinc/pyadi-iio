ea1b389edb7fdcedca84b088b913de4666dbff0d
SGudla <Saikiran.Gudla@analog.com>
2023-07-07 14:26:44 +0530

    Add support for AD5780, AD5781, AD5790, AD5791, AD5760
    Add ad578x.py
    Add a test script - test_ad578x.py
    Add an example code - ad578x_example.py
    Update index.rst
    Update init.py
    Update supported_parts.md
    Add adi.ad578x.rst
    
    Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

--- adi/__init__.py
+++ adi/__init__.py
@@ -3,6 +3,7 @@
 # SPDX short identifier: ADIBSD
 
 from adi.ad469x import ad469x
+from adi.ad578x import ad578x
 from adi.ad717x import ad717x
 from adi.ad719x import ad719x
 from adi.ad777x import ad777x
