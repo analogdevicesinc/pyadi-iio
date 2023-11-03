de9770a8e71730ce9b6ad55c98755338139f8c3c
chegbeli <ciprian.hegbeli@analog.com>
2023-11-03 11:04:32 +0200

    adi:adf4382: Fix reference_doubler_en name
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- examples/adf4382_example.py
+++ examples/adf4382_example.py
@@ -5,12 +5,12 @@
 import adi
 
 # Create pll instance connected to serial port
-pll = adi.adf4382(uri="serial:/dev/ttyACM1,115200,8n1n")
+pll = adi.adf4382(uri="serial:/dev/ttyACM0,115200,8n1n")
 
 # Configure pll attributes
 pll.sync_en = 0                         # Disable sync 
 pll.reference_frequency = 125000        # Input refernece clock in KHz
-pll.reference_doubler = 1               # Enable reference doubler
+pll.reference_doubler_en = 1            # Enable reference doubler
 pll.reference_divider = 1               # Set reference divider 
 pll.charge_pump_current = "11.100000"   # Set charge pump current in mA
 pll.bleed_current = 4903                # Set bleed current word 
