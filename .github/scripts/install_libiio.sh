1aca6691c9aed2165792f9472bd56d271b4f9b53
Travis F. Collins <travis.collins@analog.com>
2023-12-06 12:42:15 -0700

    Create derivative build for libiio v1
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- .github/scripts/install_libiio.sh
+++ .github/scripts/install_libiio.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# Set LIBIIO_BRANCH if not set
+if [ -z "$LIBIIO_BRANCH" ]; then
+    LIBIIO_BRANCH="v0.25"
+fi
+
+
 sudo apt-get -qq update
 sudo apt-get install -y git cmake graphviz libavahi-common-dev libavahi-client-dev libaio-dev libusb-1.0-0-dev libxml2-dev rpm tar bzip2 gzip flex bison git
-git clone -b 'main' --single-branch --depth 1 https://github.com/analogdevicesinc/libiio.git
+git clone -b $LIBIIO_BRANCH --single-branch --depth 1 https://github.com/analogdevicesinc/libiio.git
 cd libiio
 cmake . -DHAVE_DNS_SD=OFF
 make
@@ -11,7 +18,7 @@ rm -rf libiio
 
 # Python pieces
 sudo apt-get install -y python3-pip python3-setuptools
-git clone -b 'main' --single-branch --depth 1 https://github.com/analogdevicesinc/libiio.git
+git clone -b $LIBIIO_BRANCH --single-branch --depth 1 https://github.com/analogdevicesinc/libiio.git
 cd libiio
 cmake . -DHAVE_DNS_SD=OFF -DPYTHON_BINDINGS=ON
 make
