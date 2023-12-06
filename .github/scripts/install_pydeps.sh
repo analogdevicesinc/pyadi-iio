7dffb6bf0910b6349f22172dafb76a79aa370791
Travis F. Collins <travis.collins@analog.com>
2023-12-06 14:40:39 -0700

    Update pytest package versions
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- .github/scripts/install_pydeps.sh
+++ .github/scripts/install_pydeps.sh
@@ -2,4 +2,10 @@
 sudo apt-get install -y python3-pip python3-setuptools
 pip install -r requirements.txt
 pip install -r requirements_dev.txt
-# pip install pylibiio
+
+
+if [ "$LIBIIO_BRANCH" = "main" ]; then
+    pip install --force-reinstall pytest-libiio@git+https://github.com/tfcollins/pytest-libiio.git@libiio-v1-support
+else
+    pip install pytest-libiio==0.0.14
+fi
