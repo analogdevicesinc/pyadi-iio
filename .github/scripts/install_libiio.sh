e1bdf51ed8c868837e550e7a516ba95e9bb74f01
Travis F. Collins <travis.collins@analog.com>
2023-12-07 16:08:19 -0700

    Update dependencies for libiio v1
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- .github/scripts/install_libiio.sh
+++ .github/scripts/install_libiio.sh
@@ -7,7 +7,7 @@ fi
 
 
 sudo apt-get -qq update
-sudo apt-get install -y git cmake graphviz libavahi-common-dev libavahi-client-dev libaio-dev libusb-1.0-0-dev libxml2-dev rpm tar bzip2 gzip flex bison git
+sudo apt-get install -y git cmake graphviz libzstd-dev libavahi-common-dev libavahi-client-dev libaio-dev libusb-1.0-0-dev libxml2-dev rpm tar bzip2 gzip flex bison git
 git clone -b $LIBIIO_BRANCH --single-branch --depth 1 https://github.com/analogdevicesinc/libiio.git
 cd libiio
 cmake . -DHAVE_DNS_SD=OFF
