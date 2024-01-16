b94b62fa51b37025fad43fee5e2bb368343da1b5
Jeff Chambliss <jeff.chambliss@analog.com>
2024-01-16 15:07:13 -0500

    Added Vivado executable path and .tcl script path.  Added possible framework to run bootup command using "subprocess" module

--- test/test_triton.py
+++ test/test_triton.py
@@ -6,17 +6,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 from test.test_ad9084 import scale_field
 from test.scpi import find_instrument
+import subprocess
 
 
 hardware = ["Triton"]
 classname = "adi.Triton"
 
 #########################################
+# Boot Up Triton
+#########################################
+
+# Path to Vivado executable
+vivado_executable = r"C:\Users\JChambli\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Xilinx Design Tools\Vivado Lab 2022.2 Tcl Shell.exe"
+
+# Path to TCL script
+tcl_script_path = "C:/Users/JChambli/Downloads/vcu118_quad_ad9084_2023-09-28.tcl"
+
+# Command to run Vivado with the TCL script
+command = [vivado_executable, "-mode", "tcl", "-source", tcl_script_path]
+
+# Execute boot up command
+subprocess.run(command, check=True)
+
+#########################################
 # DC Power Test
 #########################################
 
 # # Find Keysight E36233A
-# find_instrument()
+# E36233A.channel.__init__(1)
+
+
 
 
 params = dict(
@@ -32,17 +51,6 @@ params = dict(
                         rx_dsa_gain = 0,
     ),
     loopback_test_2=dict(
-                        tx_main_nco_frequencies=[9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000],
-                        rx_main_nco_frequencies=[-3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000],
-                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_dsa_gain = 0,
-    ),
-    loopback_test_3=dict(
                         tx_main_nco_frequencies=[10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000],
                         rx_main_nco_frequencies=[-2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000],
                         tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -53,18 +61,7 @@ params = dict(
                         rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                         rx_dsa_gain = 0,
     ),
-    loopback_test_4=dict(
-                        tx_main_nco_frequencies=[11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000],
-                        rx_main_nco_frequencies=[-1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000],
-                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_dsa_gain = 0,
-    ),
-    loopback_test_5=dict(
+    loopback_test_3=dict(
                         tx_main_nco_frequencies=[12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000],
                         rx_main_nco_frequencies=[-800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000],
                         tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -153,9 +150,7 @@ def test_iio_attr(iio_uri):
     [
         (params["loopback_test_1"], 10000000, 0.9, -20, 0, 15),
         (params["loopback_test_2"], 10000000, 0.9, -20, 5, 15),
-        (params["loopback_test_3"], 10000000, 0.9, -20, 5, 15),
-        (params["loopback_test_4"], 10000000, 0.9, -20, 5, 15),
-        (params["loopback_test_5"], 10000000, 0.9, -20, 10, 15),
+        (params["loopback_test_3"], 10000000, 0.9, -20, 10, 15),
         (params["filter_test_1"], 10000000, 0.9, -20, 5, 15),
         (params["filter_test_2"], 10000000, 0.9, -30, 11, 9),
         (params["dac_adc_loopback_30dB_attenuation"], 10000000, 0.9, -50, 5, 15),
