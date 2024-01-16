7ecc969693a6b3afb3d375637c81e6a53e1e783e
Jeff Chambliss <jeff.chambliss@analog.com>
2024-01-16 09:25:50 -0500

    Added support for Keysight E36233A and reformated test_triton.py

--- test/test_triton.py
+++ test/test_triton.py
@@ -1,15 +1,80 @@
 import adi
 import test.dma_tests
 import pytest
+import test
 import matplotlib.pyplot as plt
 import numpy as np
 from test.test_ad9084 import scale_field
+from test.scpi import find_instrument
 
 
 hardware = ["Triton"]
 classname = "adi.Triton"
 
+#########################################
+# DC Power Test
+#########################################
+
+# # Find Keysight E36233A
+# find_instrument()
+
+
 params = dict(
+    loopback_test_1=dict(
+                        tx_main_nco_frequencies=[8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000, 8000000000],
+                        rx_main_nco_frequencies=[-4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000, -4800000000],
+                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_dsa_gain = 0,
+    ),
+    loopback_test_2=dict(
+                        tx_main_nco_frequencies=[9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000, 9000000000],
+                        rx_main_nco_frequencies=[-3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000, -3800000000],
+                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_dsa_gain = 0,
+    ),
+    loopback_test_3=dict(
+                        tx_main_nco_frequencies=[10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000],
+                        rx_main_nco_frequencies=[-2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000],
+                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_dsa_gain = 0,
+    ),
+    loopback_test_4=dict(
+                        tx_main_nco_frequencies=[11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000, 11000000000],
+                        rx_main_nco_frequencies=[-1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000, -1800000000],
+                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_dsa_gain = 0,
+    ),
+    loopback_test_5=dict(
+                        tx_main_nco_frequencies=[12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000, 12000000000],
+                        rx_main_nco_frequencies=[-800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000, -800000000],
+                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        rx_dsa_gain = 0,
+    ),
     filter_test_1=dict(
                         tx_main_nco_frequencies=[10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000],
                         rx_main_nco_frequencies=[-2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000],
@@ -22,17 +87,6 @@ params = dict(
                         rx_dsa_gain = 0,
     ),
     filter_test_2=dict(
-                        tx_main_nco_frequencies=[10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000],
-                        rx_main_nco_frequencies=[-2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000],
-                        tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_main_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        tx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_channel_nco_phases=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        rx_dsa_gain = 0,
-    ),
-    dac_adc_loopback_0dB_attenuation=dict(
                         tx_main_nco_frequencies=[10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000],
                         rx_main_nco_frequencies=[-2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000, -2800000000],
                         tx_channel_nco_frequencies=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -68,7 +122,10 @@ params = dict(
 )
 
 ## Set cal board state
-
+## Adjacent Loopback      CTRL_IND = 0, 5045_V1 = 1, 5045_V2 = 0, CTRL_RX_COMBINED = 0
+## Combined Loopback      CTRL_IND = 1, 5045_V1 = 1, 5045_V2 = 1, CTRL_RX_COMBINED = 0
+## Combined Tx Out/Rx In  CTRL_IND = 1, 5045_V1 = 0, 5045_V2 = 1, CTRL_RX_COMBINED = 0
+## Combined Tx RF Detect  CTRL_IND = 1, 5045_V1 = 0, 5045_V2 = 0, CTRL_RX_COMBINED = 0
 dev = adi.Triton("ip:192.168.2.1", calibration_board_attached=True)
 dev.gpio_ctrl_ind = 0
 dev.gpio_5045_v1 = 1
@@ -76,27 +133,12 @@ dev.gpio_5045_v2 = 0
 dev.gpio_ctrl_rx_combined = 0
 
 
-
+##########################################
+# Ethernet connection test
 ##########################################
 
 def test_iio_attr(iio_uri):
     print("iio_uri", iio_uri)
-
-
-# #########################################
-# @pytest.mark.iio_hardware(hardware, True)
-# @pytest.mark.parametrize("classname", [(classname)])
-# @pytest.mark.parametrize("channel", [0])
-# def test_Triton_rx_data(test_dma_rx, iio_uri, classname, channel):
-#     test_dma_rx(iio_uri, classname, channel)
-
-
-# #########################################
-# # @pytest.mark.iio_hardware(hardware)
-# @pytest.mark.parametrize("classname", [(classname)])
-# @pytest.mark.parametrize("channel", [0])
-# def test_Triton_tx_data(test_dma_tx, iio_uri, classname, channel):
-#     test_dma_tx(iio_uri, classname, channel)
 
 
 #########################################
@@ -109,13 +151,17 @@ def test_iio_attr(iio_uri):
 @pytest.mark.parametrize(
     "param_set, frequency, scale, peak_min, hpf_value, lpf_value",
     [
-        (params["filter_test_1"], 10000000, 0.75, -30, 5, 15),
-        (params["filter_test_2"], 10000000, 0.75, -50, 11, 9),
-        (params["dac_adc_loopback_0dB_attenuation"], 10000000, 0.75, -30, 5, 15),
-        (params["dac_adc_loopback_30dB_attenuation"], 10000000, 0.75, -60, 5, 15),
+        (params["loopback_test_1"], 10000000, 0.9, -20, 0, 15),
+        (params["loopback_test_2"], 10000000, 0.9, -20, 5, 15),
+        (params["loopback_test_3"], 10000000, 0.9, -20, 5, 15),
+        (params["loopback_test_4"], 10000000, 0.9, -20, 5, 15),
+        (params["loopback_test_5"], 10000000, 0.9, -20, 10, 15),
+        (params["filter_test_1"], 10000000, 0.9, -20, 5, 15),
+        (params["filter_test_2"], 10000000, 0.9, -30, 11, 9),
+        (params["dac_adc_loopback_30dB_attenuation"], 10000000, 0.9, -50, 5, 15),
     ],
 )
-def test_ad9084_dds_loopback(
+def test_Triton_dds_loopback(
     test_dds_loopback,
     iio_uri,
     classname,
@@ -128,20 +174,20 @@ def test_ad9084_dds_loopback(
     lpf_value,
 ):
     dev.hpf_ctrl = hpf_value
-    dev.lpf_ctrl = lpf_value 
+    dev.lpf_ctrl = lpf_value
     param_set = scale_field(param_set, iio_uri)
     test_dds_loopback(
         iio_uri, classname, param_set, channel, frequency, scale, peak_min
     )
 
 
-
 #########################################
 # SFDR Test 
 #########################################
+    
 @pytest.mark.parametrize("classname", [(classname)])
-@pytest.mark.parametrize("channel", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-# @pytest.mark.parametrize("channel", [0])
+# @pytest.mark.parametrize("channel", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+@pytest.mark.parametrize("channel", [0])
 @pytest.mark.parametrize(
     "param_set, hpf_value, lpf_value",
     [
@@ -152,7 +198,7 @@ def test_ad9084_dds_loopback(
 def test_Triton_sfdr(test_sfdr, iio_uri, classname, channel, param_set, sfdr_min, hpf_value, lpf_value):
     dev.hpf_ctrl = hpf_value
     dev.lpf_ctrl = lpf_value 
-    print(channel)
     test_sfdr(iio_uri, classname, channel, param_set, sfdr_min)
+
 
 
