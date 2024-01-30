042e114d6f3fd4ee36c3cc0e09feac0358663f27
chegbeli <ciprian.hegbeli@analog.com>
2024-01-30 17:41:21 +0200

    examples:adf4382: Update attributes
    The IIO attribute list has been updated, the example file has also
    been updated to match the new names and expected values.
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- examples/adf4382_example.py
+++ examples/adf4382_example.py
@@ -9,13 +9,13 @@ pll = adi.adf4382(uri="serial:/dev/ttyACM0,115200,8n1n")
 
 # Configure pll attributes
 pll.sync_en = 0                         # Disable sync 
-pll.reference_frequency = 125000        # Input refernece clock in KHz
+pll.reference_frequency = 125000000     # Input refernece clock 
 pll.reference_doubler_en = 1            # Enable reference doubler
 pll.reference_divider = 1               # Set reference divider 
 pll.charge_pump_current = "11.100000"   # Set charge pump current in mA
 pll.bleed_current = 4903                # Set bleed current word 
-pll.output_frequency = 2705000          # Output refernece clock in KHz
 
+pll.altvolt0_frequency = 3100000000       # Output refernece clock 
 pll.altvolt0_en = 1                     # Enable output channel 0
 pll.altvolt0_output_power = 9           # Set output amplitude of ch. 0
 pll.altvolt1_en = 0                     # Disable output channel 0
