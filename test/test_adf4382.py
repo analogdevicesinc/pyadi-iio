e935bd70885c5a2305b4c30b27d21b0e7474a295
chegbeli <ciprian.hegbeli@analog.com>
2024-02-07 13:52:42 +0200

    test:adf4382:Fix style
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- test/test_adf4382.py
+++ test/test_adf4382.py
@@ -11,11 +11,10 @@ classname = "adi.adf4382"
 @pytest.mark.parametrize(
     "attr, start, stop, step, tol, repeats",
     [
-        ("altvolt0_output_power", 1, 11, 1, 0, 5),#fix in driver 11 is max
-        ("altvolt1_output_power", 1, 11, 1, 0, 5),#fix in driver 11 is max
+        ("altvolt0_output_power", 1, 11, 1, 0, 5),
+        ("altvolt1_output_power", 1, 11, 1, 0, 5),
     ],
 )
-
 def test_adf4382_attrs(
     test_attribute_single_value,
     iio_uri,
@@ -30,30 +29,20 @@ def test_adf4382_attrs(
     test_attribute_single_value(
         iio_uri, classname, attr, start, stop, step, tol, repeats
     )
-    
+
+
 #########################################
 @pytest.mark.iio_hardware(hardware)
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize(
     "attr, value",
-    [
-        ("altvolt0_en", 0),
-        ("altvolt0_en", 1),
-        ("altvolt1_en", 0),
-        ("altvolt1_en", 1),
-    ],
+    [("altvolt0_en", 0), ("altvolt0_en", 1), ("altvolt1_en", 0), ("altvolt1_en", 1),],
 )
-
 def test_adf4382_attrs_bool(
-    test_attribute_single_value_boolean,
-    iio_uri,
-    classname,
-    attr,
-    value,
+    test_attribute_single_value_boolean, iio_uri, classname, attr, value,
 ):
-    test_attribute_single_value_boolean(
-        iio_uri, classname, attr, value
-    )
+    test_attribute_single_value_boolean(iio_uri, classname, attr, value)
+
 
 #########################################
 @pytest.mark.no_os_test
@@ -61,12 +50,8 @@ def test_adf4382_attrs_bool(
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize(
     "attr, start, stop, step, tol, repeats",
-    [
-        ("reference_divider", 1, 63, 1, 0, 1), #fix in driver 0 is not supported
-        ("bleed_current", 0, 8191, 100, 0, 1),
-    ],
+    [("reference_divider", 1, 63, 1, 0, 1), ("bleed_current", 0, 8191, 100, 0, 1),],
 )
-
 def test_adf4382_attrs_no_os(
     test_attribute_single_value,
     iio_uri,
@@ -82,6 +67,7 @@ def test_adf4382_attrs_no_os(
         iio_uri, classname, attr, start, stop, step, tol, repeats
     )
 
+
 #########################################
 @pytest.mark.no_os_test
 @pytest.mark.iio_hardware(hardware)
@@ -95,14 +81,7 @@ def test_adf4382_attrs_no_os(
         ("sync_en", 1),
     ],
 )
-
 def test_adf4382_attrs_bool_no_os(
-    test_attribute_single_value_boolean,
-    iio_uri,
-    classname,
-    attr,
-    value,
+    test_attribute_single_value_boolean, iio_uri, classname, attr, value,
 ):
-    test_attribute_single_value_boolean(
-        iio_uri, classname, attr, value
-    )
+    test_attribute_single_value_boolean(iio_uri, classname, attr, value)
