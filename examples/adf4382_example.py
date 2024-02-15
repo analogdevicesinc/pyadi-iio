58b348b5965ef4c2162946d95d97ed7e529d4faa
chegbeli <ciprian.hegbeli@analog.com>
2024-02-15 12:50:11 +0200

    examples:adf4382: Add debug for reg access ex.
    Added the debug example for direct register access over the IIO
    interface.
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- examples/adf4382_example.py
+++ examples/adf4382_example.py
@@ -19,3 +19,7 @@ pll.altvolt0_frequency = 3100000000  # Output reference clock
 pll.altvolt0_en = 1  # Enable output channel 0
 pll.altvolt0_output_power = 9  # Set output amplitude of ch. 0
 pll.altvolt1_en = 0  # Disable output channel 0
+
+pll.reg_write("0x0A", "0x55") # Use debug attribute to write to schratchpad
+val = pll.reg_read(0x0A) # Use debug attribute to read the schratchpad
+print(hex(int(val)))
