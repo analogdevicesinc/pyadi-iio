e0af466f9db06dc0cfc618423cf46689b8f9488d
Trisha De Vera <Trisha.Devera@analog.com>
2024-02-19 14:14:40 +0800

    Fix lint

--- test/common.py
+++ test/common.py
@@ -186,13 +186,31 @@ def dev_interface_sub_channel(
 
 
 def dev_interface_device_name_channel(
-    uri, classname, device_name, channel, val, attr, tol, sub_channel=None, sleep=0.3, readonly=False,
+    uri,
+    classname,
+    device_name,
+    channel,
+    val,
+    attr,
+    tol,
+    sub_channel=None,
+    sleep=0.3,
+    readonly=False,
 ):
     """dev_interface_device_name_channel:
     Includes device name and channel in the source to be evaluated
     """
-    
-    sdr = eval(classname + "(uri='" + uri + "', device_name='" + device_name + "').channel['" + channel + "']")
+
+    sdr = eval(
+        classname
+        + "(uri='"
+        + uri
+        + "', device_name='"
+        + device_name
+        + "').channel['"
+        + channel
+        + "']"
+    )
     # Check hardware
     if not hasattr(sdr, attr):
         raise AttributeError(attr + " not defined in " + classname)
