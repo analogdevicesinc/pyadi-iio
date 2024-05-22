83e938efb2943c4d47fc3ad61ecd7554de5b35e2
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-05-22 16:36:04 -0300

    examples: ad4020_example: Calculate voltage using driver scale attribute
    The scale may vary according to ADC characteristics such as precision
    bits, span compression, gain, etc. and also with setup properties such
    as voltage reference.
    Use driver provided scale to convert back to milli-voltage units which
    is the standard IIO unit for voltage type channels.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- examples/ad4020_example.py
+++ examples/ad4020_example.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 
-vref = 5.0  # Manually entered, consult eval board manual
-
 # Optionally pass URI as command line argument,
 # else use default context manager search
 parser = argparse.ArgumentParser(description="AD4000 Series Example Script")
@@ -59,7 +57,7 @@ print("Sample Rate: ", my_adc.sampling_frequency)
 data = my_adc.rx()
 
 x = np.arange(0, len(data))
-voltage = data * 2.0 * vref / (2 ** 20)
+voltage = data * my_adc.voltage0.scale
 dc = np.average(voltage)  # Extract DC component
 ac = voltage - dc  # Extract AC component
 
@@ -68,7 +66,7 @@ plt.clf()
 plt.title(args.device.upper() + " Time Domain Data")
 plt.plot(x, voltage)
 plt.xlabel("Data Point")
-plt.ylabel("Voltage (V)")
+plt.ylabel("Voltage (mV)")
 plt.show()
 
 f, Pxx_spec = signal.periodogram(
@@ -78,11 +76,11 @@ Pxx_abs = np.sqrt(Pxx_spec)
 
 plt.figure(2)
 plt.clf()
-plt.title(args.device.upper() + " Spectrum (Volts absolute)")
+plt.title(args.device.upper() + " Spectrum (Millivolts absolute)")
 plt.semilogy(f, Pxx_abs)
 plt.ylim([1e-6, 4])
 plt.xlabel("frequency [Hz]")
-plt.ylabel("Voltage (V)")
+plt.ylabel("Voltage (mV)")
 plt.draw()
 plt.pause(0.05)
 
