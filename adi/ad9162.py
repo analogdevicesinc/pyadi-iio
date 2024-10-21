76ec24b5a5c1d32c9a562c64d7b22709b812b273
PopPaul2021 <Paul.Pop@analog.com>
2024-10-21 16:34:41 +0300

    AD9164: Add Python example
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- adi/ad9162.py
+++ adi/ad9162.py
@@ -13,7 +13,7 @@ class ad9162(tx, context_manager, sync_start):
 
     _complex_data = False
     # _complex_data = True
-    _tx_channel_names = ["voltage0_i", "voltage0_q"]
+    _tx_channel_names = ["voltage0"]
     _device_name = ""
 
     def __init__(self, uri="", username="root", password="analog"):
@@ -21,26 +21,26 @@ class ad9162(tx, context_manager, sync_start):
         context_manager.__init__(self, uri, self._device_name)
 
         self._jesd = jesd(uri, username=username, password=password)
-        self._txdac = self._ctx.find_device("axi-ad9162-hpc")
+        self._txdac = self._ctx.find_device("axi-ad9164-hpc")
 
         tx.__init__(self)
 
     @property
     def fir85_enable(self):
-        return self._get_iio_attr("voltage0_i", "fir85_enable", True, self._txdac)
+        return self._get_iio_attr("altvoltage0", "fir85_enable", True, self._txdac)
 
     @fir85_enable.setter
     def fir85_enable(self, value):
-        self._set_iio_attr("voltage0_i", "fir85_enable", True, value, self._txdac)
+        self._set_iio_attr("voltage0", "fir85_enable", True, value, self._txdac)
 
     @property
     def sample_rate(self):
         """sample_rate: Sample frequency rate TX path in samples per second."""
-        return self._get_iio_attr("voltage0_i", "sampling_frequency", True, self._txdac)
+        return self._get_iio_attr("voltage0", "sampling_frequency", True, self._txdac)
 
     @property
     def scale(self):
-        return self._get_iio_attr("voltage0_i", "scale", True, self._txdac)
+        return self._get_iio_attr("voltage0", "scale", True, self._txdac)
 
     @property
     def frequency_nco(self):
@@ -53,3 +53,11 @@ class ad9162(tx, context_manager, sync_start):
     @property
     def jesd204_statuses(self):
         return self._jesd.get_all_statuses()
+
+    def register_read(self, reg):
+        """Direct Register Access via debugfs"""
+        self._set_iio_debug_attr_str("direct_reg_access", reg, self._txdac)
+        return self._get_iio_debug_attr_str("direct_reg_access", self._txdac)
+    def register_write(self, reg, value):
+        """Direct Register Access via debugfs"""
+        self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._txdac)
