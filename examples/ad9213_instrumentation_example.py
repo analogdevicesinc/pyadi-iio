b50ab9b4997b9b833d94e80d62841b81ca1557f0
PopPaul2021 <Paul.Pop@analog.com>
2024-11-13 17:05:23 +0200

    Adding SYNC_N GPIO and lvds_sync attribute
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/ad9213_instrumentation_example.py
+++ examples/ad9213_instrumentation_example.py
@@ -19,16 +19,17 @@ print("uri: " + str(my_uri))
 # Create contexts
 
 ad9213_transport = adi.ad9213(uri=my_uri,device_name="axi-adrv9213-rx-hpc")
-ad9213_phy       = adi.ad9213(uri=my_uri,device_name="ad9213") 
+ad9213_phy       = adi.ad9213(uri=my_uri,device_name="ad9213")
 ad4080_dev       = adi.ad4080(uri=my_uri)
 gpio_controller  = adi.one_bit_adc_dac(uri=my_uri, name="one-bit-adc-dac")
 ltc2664_dev      = adi.ltc2664(uri=my_uri)
 
 # Control the GPIO's
 
-gpio_controller.gpio_ltc2664_clr  = 1
-gpio_controller.gpio_ltc2664_ldac = 0
-gpio_controller.gpio_ltc2664_tgp  = 0
+gpio_controller.gpio_ad4080_sync_n  = 0
+gpio_controller.gpio_ltc2664_clr    = 1
+gpio_controller.gpio_ltc2664_ldac   = 0
+gpio_controller.gpio_ltc2664_tgp    = 0
 
 gpio_controller.gpio_ada4945_disable  = 1
 gpio_controller.gpio_adg5419_ctrl     = 1
@@ -44,21 +45,21 @@ ad9213_transport.rx_buffer_size = 8192
 ad9213_phy.ad9213_register_write(0x1617,0x01)
 ad9213_phy.ad9213_register_write(0x1601,0x01)
 
-# Configure the AD4080 device 
+# Configure the AD4080 device
 
 ad4080_dev.rx_buffer_size = 8192
+ad4080_dev.lvds_sync = "enable"
+print(ad4080_dev.lvds_sync)
 
-# ad4080_dev.lvds_sync = "enable"
+# Configure the LTC2664 device
 
-# Configure the LTC2664 device 
+ltc2664_dev.voltage0.raw = 32768
 
-ltc2664_dev.voltage0.raw = 32768  
-
-ltc2664_dev.voltage1.raw = 49152 
+ltc2664_dev.voltage1.raw = 49152
 print(f"ltc2664_dev.voltage1 is {ltc2664_dev.voltage1.volt}")
 ltc2664_dev.voltage2.raw = 36045
 print(f"ltc2664_dev.voltage2 is {ltc2664_dev.voltage2.volt}")
-# rx data 
+# rx data
 
 data_ad9213 = ad9213_transport.rx()
 data_ad4080 = ad4080_dev.rx()
