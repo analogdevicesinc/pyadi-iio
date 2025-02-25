0f327fbb95ee7e3757723a51f350b51aeda33edd
PopPaul2021 <Paul.Pop@analog.com>
2025-02-25 17:18:55 +0200

    update maximum sampling_rate and update the modulated data
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/adrv9002_example.py
+++ examples/adrv9002_example.py
@@ -7,34 +7,10 @@ import commpy.utilities as util
 import komm
 import scipy.io
 
-def rcosdesign(beta, span, sps, N=None):
-    """
-    Generate a raised cosine (RC) filter (FIR) impulse response.
+sdr = adi.adrv9002(uri="ip:10.48.65.203")
 
-    Parameters:
-    beta (float): Roll-off factor (0 <= beta <= 1).
-    span (int): Number of symbol periods the filter spans.
-    sps (int): Samples per symbol.
-    N (int, optional): Number of taps in the filter. If None, it defaults to span * sps + 1.
+sdr.write_stream_profile( "lte_40_lvds_api_68_14_10.stream" ,"lte_40_lvds_api_68_14_10.json")
 
-    Returns:
-    np.ndarray: Impulse response of the raised cosine filter.
-    """
-    if N is None:
-        N = span * sps + 1
-
-    t = np.linspace(-span / 2, span / 2, N)
-    h = np.sinc(t) * np.cos(np.pi * beta * t) / (1 - (2 * beta * t)**2)
-
-    # Handle the singularity at t = 0
-    h[t == 0] = 1.0
-    # Handle the singularity at t = ±1/(2*beta)
-    singularity_indices = np.isclose(t, 1 / (2 * beta)) | np.isclose(t, -1 / (2 * beta))
-    h[singularity_indices] = beta / (2 * np.sqrt(2)) * ((1 + 2 / np.pi) * np.sin(np.pi / (2 * beta)) + (1 - 2 / np.pi) * np.cos(np.pi / (2 * beta)))
-
-    return h
-
-sdr = adi.adrv9002(uri="ip:10.48.65.154")
 sdr.tx_ensm_mode_chan0 = "rf_enabled"
 sdr.tx_ensm_mode_chan1 = "rf_enabled"
 sdr.tx_cyclic_buffer = True
@@ -69,18 +45,14 @@ modulation_map = {
     "11": "ORIGINAL"
 }
 
-filterCoeffs = rcosdesign(0.35, 4, sps)
-M = 2
-
-
-mod_8spk  = scipy.io.loadmat('modulated_data/mod_8PSK.mat')
-mod_16qam = scipy.io.loadmat('modulated_data/mod_16QAM.mat')
-mod_64qam = scipy.io.loadmat('modulated_data/mod_64QAM.mat')
-mod_bspk  = scipy.io.loadmat('modulated_data/mod_BPSK.mat')
-mod_cpfsk = scipy.io.loadmat('modulated_data/mod_CPFSK.mat')
-mod_gfsk  = scipy.io.loadmat('modulated_data/mod_GFSK.mat')
-mod_pam4  = scipy.io.loadmat('modulated_data/mod_PAM4.mat')
-mod_qpsk  = scipy.io.loadmat('modulated_data/mod_QPSK.mat')
+mod_8spk  = scipy.io.loadmat('modulated_data/input_mod_8PSK.mat')
+mod_16qam = scipy.io.loadmat('modulated_data/input_mod_16QAM.mat')
+mod_64qam = scipy.io.loadmat('modulated_data/input_mod_64QAM.mat')
+mod_bspk  = scipy.io.loadmat('modulated_data/input_mod_BPSK.mat')
+mod_cpfsk = scipy.io.loadmat('modulated_data/input_mod_CPFSK.mat')
+mod_gfsk  = scipy.io.loadmat('modulated_data/input_mod_GFSK.mat')
+mod_pam4  = scipy.io.loadmat('modulated_data/input_mod_PAM4.mat')
+mod_qpsk  = scipy.io.loadmat('modulated_data/input_mod_QPSK.mat')
 
 while True:
     modulation_number = input("Enter modulation type (1: 16QAM, 2: 64QAM, 3: 8PSK, 5: BPSK, 6: CPFSK, 8: GFSK, 9: PAM4, 10: QPSK, 11: ORIGINAL 0:EXIT): ")
@@ -91,23 +63,23 @@ while True:
 
     match modulation_type:
         case "BPSK":
-            data = mod_bspk['rx']
+            data = mod_bspk['yc']
         case "QPSK":
-            data = mod_qpsk['rx']
+            data = mod_qpsk['yc']
         case "8PSK":
-            data = mod_8spk['rx']
+            data = mod_8spk['yc']
         case "16QAM":
-            data = mod_16qam['rx']
+            data = mod_16qam['yc']
         case "64QAM":
-            data = mod_64qam['rx']
+            data = mod_64qam['yc']
         case "PAM4":
-            data = mod_pam4['rx']
+            data = mod_pam4['yc']
         case "GFSK":
-            data = mod_gfsk['rx']
+            data = mod_gfsk['yc']
         case "CPFSK":
-            data = mod_cpfsk['rx']
+            data = mod_cpfsk['yc']
         case "ORIGINAL":
-            iq = i + 1j * q
+            data = i + 1j * q
         case "EXIT":
             sdr.tx_destroy_buffer()
             break
@@ -120,13 +92,6 @@ while True:
     iq_imag = np.int16(np.imag(data) * 2**12-1)
     iq = iq_real + 1j * iq_imag
     sdr.tx(iq)
-    # plt.figure()
-    # plt.subplot(2, 1, 1)
-    # plt.plot(data)
-    # plt.subplot(2, 1, 2)
-    # plt.plot(iq)
-    # plt.tight_layout()
-    # plt.show()
 
 
 
