9a348d309aa301cef4c1be6fc76957dd8bf2d4a5
PopPaul2021 <Paul.Pop@analog.com>
2025-02-26 18:46:56 +0200

    working version
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/adrv9002_example.py
+++ examples/adrv9002_example.py
@@ -7,30 +7,46 @@ import commpy.utilities as util
 import komm
 import scipy.io
 
-sdr = adi.adrv9002(uri="ip:10.48.65.203")
+sdr  = adi.adrv9002(uri="ip:10.48.65.203")
+
 
 sdr.write_stream_profile( "lte_40_lvds_api_68_14_10.stream" ,"lte_40_lvds_api_68_14_10.json")
-
+sdr.tx0_port_en = "spi"
+sdr.tx1_port_en = "spi"
 sdr.tx_ensm_mode_chan0 = "rf_enabled"
 sdr.tx_ensm_mode_chan1 = "rf_enabled"
 sdr.tx_cyclic_buffer = True
+sdr.tx2_cyclic_buffer = True
 sdr.tx0_lo = 2400000000
 sdr.tx1_lo = 2400000000
 sdr.tx_hardwaregain_chan0 = -10
 sdr.tx_hardwaregain_chan1 = -10
-sdr.tx_enabled_channels=[0]
-fs = int(sdr.rx0_sample_rate)
+
+
+sdr1 = adi.adrv9002(uri="ip:10.48.65.154")
+sdr1.write_stream_profile( "lte_40_lvds_api_68_14_10.stream" ,"lte_40_lvds_api_68_14_10.json")
+
+sdr1.tx0_port_en = "spi"
+sdr1.tx1_port_en = "spi"
+sdr1.tx_ensm_mode_chan0 = "rf_enabled"
+sdr1.tx_ensm_mode_chan1 = "rf_enabled"
+sdr1.tx_cyclic_buffer = True
+sdr1.tx2_cyclic_buffer = True
+sdr1.tx0_lo = 2400000000
+sdr1.tx1_lo = 2400000000
+sdr1.tx_hardwaregain_chan0 = -10
+sdr1.tx_hardwaregain_chan1 = -10
+
+
+fs = int(sdr1.tx0_sample_rate)
 
 print(fs)
 fc = 50000
-N = 1024
+N = 49120
 ts = 1 / float(fs)
 t = np.arange(0, N * ts, ts)
 i = np.sin(2 * np.pi * fc * t)
 q = np.cos(2 * np.pi * fc * t)
-
-spf = 2*N  # samples per frame
-sps = 8  # samples per symbol
 
 modulation_map = {
     "0": "EXIT",
@@ -55,8 +71,14 @@ mod_pam4  = scipy.io.loadmat('modulated_data/input_mod_PAM4.mat')
 mod_qpsk  = scipy.io.loadmat('modulated_data/input_mod_QPSK.mat')
 
 while True:
+
     modulation_number = input("Enter modulation type (1: 16QAM, 2: 64QAM, 3: 8PSK, 5: BPSK, 6: CPFSK, 8: GFSK, 9: PAM4, 10: QPSK, 11: ORIGINAL 0:EXIT): ")
     sdr.tx_destroy_buffer()
+    sdr.tx2_destroy_buffer()
+    sdr.tx2_destroy_buffer()
+    sdr1.tx_destroy_buffer()
+    sdr1.tx2_destroy_buffer()
+    sdr1.tx2_destroy_buffer()
     print(modulation_number)
     modulation_type = modulation_map.get(modulation_number, "BPSK")
     print(modulation_type)
@@ -82,25 +104,21 @@ while True:
             data = i + 1j * q
         case "EXIT":
             sdr.tx_destroy_buffer()
-            break
-        case _:
-            sdr.tx_destroy_buffer()
+            sdr.tx2_destroy_buffer()
+
+            sdr1.tx_destroy_buffer()
+            sdr1.tx2_destroy_buffer()
             break
 
     data=data.flatten()
     iq_real = np.int16(np.real(data) * 2**12-1)
     iq_imag = np.int16(np.imag(data) * 2**12-1)
     iq = iq_real + 1j * iq_imag
+
     sdr.tx(iq)
-
-
-
-
-
-
-
-
-
-
-
-
+    time.sleep(0.5)
+    sdr.tx2(iq)
+    time.sleep(0.5)
+    sdr1.tx(iq)
+    time.sleep(0.5)
+    sdr1.tx2(iq)
