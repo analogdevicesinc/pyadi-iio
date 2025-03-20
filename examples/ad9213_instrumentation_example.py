9cfc50374f733d70735619f95b3fd5e6f4a0e144
PopPaul2021 <Paul.Pop@analog.com>
2025-03-20 15:59:33 +0200

    working sync v1
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/ad9213_instrumentation_example.py
+++ examples/ad9213_instrumentation_example.py
@@ -32,7 +32,13 @@ multi = adi.ad9213_instr(primary, [], primary_jesd, [])
 multi._dma_show_arming = False
 multi._jesd_show_status = True
 multi._jesd_fsm_show_status = True
-multi.rx_buffer_size = 2 ** 15
+multi.primary.rx_buffer_size = 2 ** 17
+multi.secondaries[0].rx_buffer_size = 2 ** 17
+
+print(multi.primary.rx_buffer_size)
+print(multi.secondaries[0].rx_buffer_size)
+
+multi.primary._clock_chip_fmc.reg_write(0x5A,0x1)
 
 # Control the GPIO's
 stdout, stderr = ssh._run(f"busybox devmem 0x44A00068 32 ")
@@ -62,49 +68,68 @@ if stdout == "0x00000000":
 
     gpio_controller.gpio_hmc7044_sync_req = 0
     print("AD4080 needs to be synced")
+    time.sleep(1)
     ad4080_dev.lvds_sync = "enable"
 else:
     print("AD4080 is already synced")
 
 
-data = np.array(multi.rx())
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450088 32 ")
+print(f"0x88 is: {stdout}")
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450088 32 0x2")
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450088 32 ")
+print(f"0x88 is: {stdout}")
+stdout, stderr = ssh._run(f"busybox devmem 0x44A30010 32 ")
+print(f"0x10 is: {stdout}")
 
-ad4080_interpolated = np.interp(
-    np.arange(0, len(data[1]) * 320, 1),
-    np.arange(0, len(data[1]) * 320, 320),
-    -data[1]
-)
-ad4080_interpolated = ad4080_interpolated[:32768]
-# Upscale ad4080_interpolated to match data[0] amplitude and offset
-ad4080_interpolated = ad4080_interpolated * (np.max(data[0]) / np.max(ad4080_interpolated))
-ad4080_interpolated = ad4080_interpolated + (np.mean(data[0]) - np.mean(ad4080_interpolated))
 
-cross_corr = np.correlate(data[0], ad4080_interpolated, mode='full')
-#
-coarse_phase_shift = np.argmax(cross_corr) - (len(ad4080_interpolated) - 1)
-print(f"Coarse phase shift: {coarse_phase_shift}")
-#
-# # Fine-tune the coarse phase shift
-fine_tuned_shift = coarse_phase_shift
-max_corr_value = cross_corr[coarse_phase_shift + len(ad4080_interpolated) - 1]
 
-for shift in range(coarse_phase_shift - 320, coarse_phase_shift + 321):
-    if 0 <= shift < len(cross_corr):
-        if cross_corr[shift + len(ad4080_interpolated) - 1] > max_corr_value:
-            max_corr_value = cross_corr[shift + len(ad4080_interpolated) - 1]
-            fine_tuned_shift = shift
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450104 32 ")
+print(f"0x104 is: {stdout}")
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450104 32 0x1")
 
-print(f"Fine-tuned phase shift: {fine_tuned_shift}")
+stdout, stderr = ssh._run(f"busybox devmem 0x7c450104 32 ")
+print(f"0x104 is: {stdout}")
+
 
 plt.figure()
-plt.subplot(2, 1, 1)
-plt.plot(data[0], label="AD9213")
-plt.plot(ad4080_interpolated, label="AD4080")
-plt.legend()
 
-plt.subplot(2, 1, 2)
-plt.plot(cross_corr, label="Cross corelated signal")
-plt.legend()
+for i in range(1):
+    data1, data2 = multi.rx()
+    print(data1.shape)
+    print(data2.shape)
+    ad4080_interpolated = np.interp(
+    np.arange(0, len(data2) * 320, 1),
+    np.arange(0, len(data2) * 320, 320),
+    -data2
+    )
+    ad4080_interpolated = ad4080_interpolated[:multi.primary.rx_buffer_size]
+    # Upscale ad4080_interpolated to match data1 amplitude and offset
+    ad4080_interpolated = ad4080_interpolated * (np.max(data1) / np.max(ad4080_interpolated))
+    ad4080_interpolated = ad4080_interpolated + (np.mean(data1) - np.mean(ad4080_interpolated))
+    #
+    cross_corr = np.correlate(data1, ad4080_interpolated, mode='full')
+    #
+    coarse_phase_shift = np.argmax(cross_corr) - (len(ad4080_interpolated) - 1)
+    print(f"Coarse phase shift: {coarse_phase_shift}")
+    #
+    # # Fine-tune the coarse phase shift
+    fine_tuned_shift = coarse_phase_shift
+    max_corr_value = cross_corr[coarse_phase_shift + len(ad4080_interpolated) - 1]
+    #
+    for shift in range(coarse_phase_shift - 320, coarse_phase_shift + 321):
+        if 0 <= shift < len(cross_corr):
+            if cross_corr[shift + len(ad4080_interpolated) - 1] > max_corr_value:
+                max_corr_value = cross_corr[shift + len(ad4080_interpolated) - 1]
+                fine_tuned_shift = shift
+    #
+    print(f"Fine-tuned phase shift: {fine_tuned_shift}")
+    plt.subplot(2, 1, 1)
+    plt.plot(data1, label="AD9213")
+    plt.plot(ad4080_interpolated, label="AD4080")
+    plt.subplot(2, 1, 2)
+    plt.plot(cross_corr, label="Cross corelated signal")
+    multi.rx_destroy_buffer()
 
 plt.show()
 
