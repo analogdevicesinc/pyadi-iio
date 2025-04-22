9dad3fe8ccbf90adf6756c51e1e214a9540c9f6d
Macy Libed <macy.libed@analog.com>
2025-04-22 10:27:31 +0800

    Simplify temp attribute
    Signed-off-by: Macy Libed <macy.libed@analog.com>

--- test/test_adt7420.py
+++ test/test_adt7420.py
@@ -10,10 +10,10 @@ classname = "adi.adt7420"
 @pytest.mark.parametrize(
     "attr, start, stop, step, tol, repeats, sub_channel",
     [
-        ("temp_max", 5, 50, 5, 1, 2, "_channel"),
-        ("temp_min", 0, 40, 5, 1, 1, "_channel"),
-        ("temp_crit", 80, 150, 5, 10, 1, "_channel"),
-        ("temp_hyst", 5, 20, 1, 1, 1, "_channel"),
+         ("temp_max", 5, 50, 5, 5, 1, "temp"),
+        ("temp_min", 0, 40, 5, 5, 1, "temp"),
+        ("temp_crit", 80, 150, 5, 10, 1, "temp"),
+        ("temp_hyst", 20, 40, 5, 20, 1, "temp"),
     ],
 )
 def test_adt7420_attrs(
