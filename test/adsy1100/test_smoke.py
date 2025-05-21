0ecda231ba1e1cb2c87fac04fe998bb6cc8d071a
Travis F. Collins <travis.collins@analog.com>
2025-05-20 19:16:24 -0600

    update logging
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/adsy1100/test_smoke.py
+++ test/adsy1100/test_smoke.py
@@ -160,9 +160,10 @@ def nebula_boot_adsy1100_ethernet(request, power_supply, record_property):
                     )
 
         # Reboot
+        neb_manager.monitor[0].start_log(logappend=True)
         neb_manager.monitor[0].print_to_console = show_uart_log
         neb_manager.monitor[0]._read_until_stop()  # Flush
-        neb_manager.monitor[0].start_log(logappend=True)
+        do_flush(neb_manager.monitor[0])
         neb_manager.net.reboot_board(bypass_sleep=True)
 
         # Wait for Linux login
