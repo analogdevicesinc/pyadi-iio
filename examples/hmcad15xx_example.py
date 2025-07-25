af4ba788873d39aefb1adf69a3c0bee9a1843a4b
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2025-07-25 13:32:48 +0300

    HMCAD15xx: Initial pattern check mechansim
    Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

--- examples/hmcad15xx_example.py
+++ examples/hmcad15xx_example.py
@@ -20,7 +20,7 @@ print("uri: " + str(my_uri))
 
 hmcad15xx_dev = adi.hmcad15xx(uri=my_uri)
 
-hmcad15xx_dev.rx_buffer_size = 8096
+hmcad15xx_dev.rx_buffer_size = 65536
 hmcad15xx_dev.rx_enabled_channels = [0,1,2,3]
 print("RX rx_enabled_channels: " + str(hmcad15xx_dev.rx_enabled_channels))
 
@@ -46,13 +46,40 @@ hmcad15xx_dev.channel[3].input_select = "IP4_IN4"
 print("Signal input 0X3A is:", hmcad15xx_dev.hmcad15xx_register_read(0x3A))
 print("Signal input 0X3B is:", hmcad15xx_dev.hmcad15xx_register_read(0x3B))
 
-print("Writing 0xABCD to 0x26 custom pattern")
-hmcad15xx_dev.hmcad15xx_register_write(0x26,0xABCD)
-print("0x26 custom pattern is:", hmcad15xx_dev.hmcad15xx_register_read(0x26))
+# print("Writing 0xABCD to 0x26 custom pattern")
+# hmcad15xx_dev.hmcad15xx_register_write(0x26,0xABCD)
+# print("0x26 custom pattern is:", hmcad15xx_dev.hmcad15xx_register_read(0x26))
+
+# print("Test single custom pattern")
+
+# # write the desired custom pattern by setting the register 0x25 with value 0x10
+# # this signals that the tested value is user specified
+# custom_pattern = 0xAC5D
+# test_pattern = 0x40
+# print(f"Writting single custom pattern: {hex(custom_pattern)}")
+# hmcad15xx_dev.hmcad15xx_register_write(0x25, test_pattern)
+# # specify the custom value
+# hmcad15xx_dev.hmcad15xx_register_write(0x26, custom_pattern)
+
+# Change output mode from 2's complement
+hmcad15xx_dev.hmcad15xx_register_write(0x46, 0x4)
+# Test deskew and sync patterns
+hmcad15xx_dev.hmcad15xx_register_write(0x45, 0x02)
 
 # rx data
 
 data = hmcad15xx_dev.rx()
+
+# test the speciefied pattern
+print(f"Read first value: {hex(data[0][0])}")
+print(f"Read second value: {hex(data[0][1])}")
+
+# read from register map
+ssh = adi.sshfs(address=my_uri, username="root", password="analog")
+base_addr   = 0x44A00000
+control_reg = 0x404
+stdout, stderr = ssh._run(f"busybox devmem {base_addr + control_reg} 16")
+print(stdout)
 
 # plot setup
 
