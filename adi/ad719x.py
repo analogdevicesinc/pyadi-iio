analogdevicesinc/pyadi-iio

commit 33aeb03c3f0e13693ee24ede2d78079a2adcce61

projects:ad7191_iio:Added the python support for ad7191
1) Modified to_volts so that it gives the correct value for both
   unipolar and bipolar mode

Signed-off-by: Kantipudi-Supriya <supriya.kantipudi@analog.com>

adi/ad719x.py
@@ -23,7 +23,7 @@ class ad719x(rx, context_manager):
         """Constructor for AD719x class."""
         context_manager.__init__(self, uri, self._device_name)
 
-        compatible_parts = ["ad7190", "ad7192", "ad7193", "ad7194", "ad7195"]
+        compatible_parts = ["ad7190", "ad7191","ad7192", "ad7193", "ad7194", "ad7195"]
 
         self._ctrl = None
 
@@ -89,14 +89,15 @@ class ad719x(rx, context_manager):
     def to_volts(self, index, val):
         """Converts raw value to SI."""
         _scale = self.channel[index].scale
+        _offset = self.channel[index].offset
 
         ret = None
 
         if isinstance(val, np.int32):
-            ret = val * _scale
+            ret = (val + _offset) / 1000 * _scale
 
         if isinstance(val, np.ndarray):
-            ret = [x * _scale for x in val]
+            ret = [((x + _offset) / 1000) * _scale for x in val]
 
         if ret is None:
             raise Exception("Error in converting to actual voltage")

commit 9ab077e92dde5407cda0b5f385a625f069d76939

adi: ad719x: add missing whitespace in array
Fix the lint build by adding the missing whitespace after ','.

Fixes: 33aeb03c3f0e ("projects:ad7191_iio:Added the python support for ad7191")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

adi/ad719x.py
@@ -24,7 +24,7 @@ class ad719x(rx, context_manager):
         """Constructor for AD719x class."""
         context_manager.__init__(self, uri, self._device_name)
 
-        compatible_parts = ["ad7190", "ad7191","ad7192", "ad7193", "ad7194", "ad7195"]
+        compatible_parts = ["ad7190", "ad7191", "ad7192", "ad7193", "ad7194", "ad7195"]
 
         self._ctrl = None
 