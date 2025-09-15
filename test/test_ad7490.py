7a1dda1b2bebb277122d3cc8380f841e03e2c35f
Travis F. Collins <travis.collins@analog.com>
2025-09-15 12:57:02 -0400

    Fix lint issue
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/test_ad7490.py
+++ test/test_ad7490.py
@@ -17,10 +17,7 @@ def test_ad7490_rx_data(test_dma_rx, iio_uri, classname, channel):
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize(
     "attr, val",
-    [
-        ("polarity", ["BIPOLAR", "UNIPOLAR"],),
-        ("range", ["2X_REF_IN", "REF_IN"],),
-    ],
+    [("polarity", ["BIPOLAR", "UNIPOLAR"],), ("range", ["2X_REF_IN", "REF_IN"],),],
 )
 def test_ad7490_attr_multiple(
     test_attribute_multiple_values, iio_uri, classname, attr, val
