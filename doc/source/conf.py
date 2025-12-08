3ed543fa6aa05163ddd4a17f43b8b3854c79b5b7
Travis F. Collins <travis.collins@analog.com>
2025-12-08 09:57:08 -0500

    Fix broken doc links
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -65,6 +65,10 @@ coverage_show_missing_items = True
 coverage_ignore_classes = ["phy"]
 coverage_ignore_modules = ["test.dma_tests", "test.generics"]
 
+# Link check configuration
+linkcheck_ignore = [
+    r"_static/logos.*",
+]
 
 # -- External docs configuration ----------------------------------------------
 
