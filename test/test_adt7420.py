0a46730dc6b060c262dd39946387024f3352d11a
Travis Collins <travis.collins@analog.com>
2026-01-05 16:49:49 +0000

    Fix test lint error
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- test/test_adt7420.py
+++ test/test_adt7420.py
@@ -10,7 +10,7 @@ classname = "adi.adt7420"
 @pytest.mark.parametrize(
     "attr, start, stop, step, tol, repeats, sub_channel",
     [
-         ("temp_max", 5, 50, 5, 5, 1, "temp"),
+        ("temp_max", 5, 50, 5, 5, 1, "temp"),
         ("temp_min", 0, 40, 5, 5, 1, "temp"),
         ("temp_crit", 80, 150, 5, 10, 1, "temp"),
         ("temp_hyst", 20, 40, 5, 20, 1, "temp"),
