65977ea86f9d71ceba4bfce0c3515fe59ed9a862
Jorge Marques <jorge.marques@analog.com>
2026-02-16 17:21:25 +0100

    dirty: ad9084: buffer cli and debugfs
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

---
+++ test/test_ad9084_buffer_cli.py
@@ -0,0 +1,32 @@
+import os
+import struct
+import subprocess
+import shutil
+
+import iio
+import numpy as np
+import pytest
+
+hardware = ["adsy1100"]
+uri = os.getenv("TEST_URI", "ip:10.44.3.132")
+
+
+@pytest.mark.iio_hardware(hardware)
+def test_ad9084_buffer_cli():
+    os.makedirs("sample", exist_ok=True)
+
+    dac_buffer_cli = shutil.which("dac_buffer_cli")
+    assert dac_buffer_cli, "No dac_buffer_cli found!"
+
+    subprocess.run(
+        [
+            dac_buffer_cli,
+            "-u",
+            uri,
+            "-d",
+            "axi-ad9084-tx-hpc",
+            "-f",
+            "/mnt/wsl/data/repos/iio-oscilloscope/waveforms/sinewave_0.9.mat",
+        ],
+        check=True,
+    )
--- test/test_ad9084_rx_capture.py
+++ test/test_ad9084_rx_capture.py
@@ -8,10 +8,10 @@ import numpy as np
 import pytest
 
 hardware = ["adsy1100"]
-uri = "ip:10.44.3.92"
+uri = "ip:10.44.3.116"
 
-n = 1562
-m = 1228
+n = 5
+m = 4
 
 @pytest.mark.iio_hardware(hardware)
 def test_ad9084_rx_capture():
@@ -29,14 +29,26 @@ def test_ad9084_rx_capture():
     ratio_results = []
 
     mask = iio.ChannelsMask(rx_dev)
+    # Enable all channels, some are not yet considered supported.
     mask.channels = [
         rx_dev.find_channel("voltage0_i"),
         rx_dev.find_channel("voltage0_q"),
+        rx_dev.find_channel("voltage1_i"),
+        rx_dev.find_channel("voltage1_q"),
+        rx_dev.find_channel("voltage2_i"),
+        rx_dev.find_channel("voltage2_q"),
+        rx_dev.find_channel("voltage3_i"),
+        rx_dev.find_channel("voltage3_q"),
     ]
     buf = iio.Buffer(rx_dev, mask)
     stream = iio.Stream(buffer=buf, nb_blocks=1, samples_count=1024)
 
     fsrc_ch = fsrc_dev.find_channel("altvoltage0", True)
+
+    rx_dev.debug_attrs['fsrc_configure_rx'].value = f"{n} {m}"
+    rx_dev.debug_attrs['fsrc_configure_tx'].value = f"{n} {m}"
+    rx_dev.debug_attrs['fsrc_rx_reconfig_spi'].value = "1"
+    rx_dev.debug_attrs['fsrc_tx_reconfig_spi'].value = "1"
 
     for rx_state in [1, 0]:
         fsrc_ch.attrs["rx_enable"].value = str(rx_state)
@@ -83,4 +95,6 @@ def test_ad9084_rx_capture():
 
     ratio_file = "sample_rx_capture/sample_ratio.txt"
     with open(ratio_file, "w") as f:
-        f.write("\n".join(result) + "\n")
+        f.write(result + "\n")
+
+    assert abs(ratio_exp - ratio_pra) < 0.05, "ratios are too distant!"
