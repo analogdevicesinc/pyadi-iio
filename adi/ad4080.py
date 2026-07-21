analogdevicesinc/pyadi-iio

commit 47913405e07af867a9f4398d96f86904db2f6d46

ad4080: add pyadi-iio support
Add support for ad4080.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

adi/ad4080.py
@@ -1,0 +1,107 @@
+# Copyright (C) 2025 Analog Devices, Inc.
+#
+# SPDX short identifier: ADIBSD
+
+from adi.attribute import attribute
+from adi.context_manager import context_manager
+from adi.rx_tx import rx
+
+
+class ad4080(rx, context_manager):
+
+    """ AD4080 ADC """
+
+    _complex_data = False
+    _device_name = ""
+    channel = []  # type: ignore
+
+    def __init__(self, uri="", device_name="ad4080"):
+        context_manager.__init__(self, uri, self._device_name)
+
+        compatible_part = "ad4080"
+        self._ctrl = None
+
+        if not device_name:
+            device_name = compatible_part
+        else:
+            if device_name != compatible_part:
+                raise Exception(f"Not a compatible device: {device_name}")
+
+        # Select the device matching device_name as working device
+        for device in self._ctx.devices:
+            if device.name == device_name:
+                self._ctrl = device
+                self._rxadc = device
+                break
+
+        # Raise an exception if the device isn't found
+        if not self._ctrl:
+            raise Exception("AD4080 device not found")
+
+        if not self._rxadc:
+            raise Exception("Error in selecting matching device")
+
+        self._rx_channel_names = []
+        self.channel = []
+        for ch in self._ctrl.channels:
+            name = ch.id
+            self._rx_channel_names.append(name)
+            self.channel.append(self._channel(self._ctrl, name))
+
+        rx.__init__(self)
+
+    class _channel(attribute):
+        """AD4080 channel"""
+
+        def __init__(self, ctrl, channel_name):
+            self.name = channel_name
+            self._ctrl = ctrl
+
+        @property
+        def scale(self):
+            """Get Scale value"""
+            return self._get_iio_attr("voltage0", "scale", False)
+
+    @property
+    def sampling_frequency(self):
+        """Get Sampling frequency value"""
+        return self._get_iio_dev_attr("sampling_frequency", False)
+
+    @property
+    def oversampling_ratio_available(self):
+        """Get the oversampling ratio available values"""
+        return self._get_iio_dev_attr("oversampling_ratio_available", False)
+
+    @property
+    def oversampling_ratio(self):
+        """Get the oversampling ratio value"""
+        return self._get_iio_dev_attr("oversampling_ratio", False)
+
+    @oversampling_ratio.setter
+    def oversampling_ratio(self, value):
+        """Set the oversampling ratio value"""
+        self._set_iio_dev_attr("oversampling_ratio", value)
+
+    @property
+    def filter_type_available(self):
+        """Get the filter type available values"""
+        return self._get_iio_dev_attr_str("filter_type_available", False)
+
+    @property
+    def filter_type(self):
+        """Get the filter type value"""
+        return self._get_iio_dev_attr_str("filter_type", False)
+
+    @filter_type.setter
+    def filter_type(self, value):
+        """Set the filter type value"""
+        self._set_iio_dev_attr_str("filter_type", value)
+
+    def reg_read(self, reg):
+        """Direct Register Access via debugfs"""
+        self._set_iio_debug_attr_str("direct_reg_access", reg, self._ctrl)
+        return self._get_iio_debug_attr_str("direct_reg_access", self._ctrl)
+
+    def reg_write(self, reg, value):
+        """Direct Register Access via debugfs"""
+        self._set_iio_debug_attr_str("direct_reg_access", f"{reg} {value}", self._ctrl)

adi/__init__.py
@@ -16,6 +16,7 @@ from adi.ad937x import ad9371, ad9375
 from adi.ad3552r import ad3552r
 from adi.ad3552r_hs import ad3552r_hs
 from adi.ad4020 import ad4000, ad4001, ad4002, ad4003, ad4020
+from adi.ad4080 import ad4080
 from adi.ad4110 import ad4110
 from adi.ad4130 import ad4130
 from adi.ad4170 import ad4170

examples/ad4080_example.py
@@ -1,0 +1,70 @@
+# Copyright (C) 2025 Analog Devices, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from adi import ad4080
+
+# Optionally pass URI as command line argument,
+# else use default ip:analog.local
+my_uri = sys.argv[1] if len(sys.argv) >= 2 else "ip:analog.local"
+print("uri: " + str(my_uri))
+
+my_adc = ad4080(uri=my_uri)
+
+print("Sampling frequency: ", my_adc.sampling_frequency)
+print("Filter type: ", my_adc.filter_type)
+print("Scale: ", my_adc.channel[0].scale)
+print("Oversampling ratio: ", my_adc.oversampling_ratio)
+
+plt.clf()
+# Collect data
+data = my_adc.rx()
+
+plt.plot(range(0, len(data)), data, label="channel0")
+plt.xlabel("Data Point")
+plt.ylabel("ADC counts")
+plt.legend(
+    bbox_to_anchor=(0.0, 1.02, 1.0, 0.102),
+    loc="lower left",
+    ncol=4,
+    mode="expand",
+    borderaxespad=0.0,
+)
+
+plt.show()
+
+del my_adc

test/emu/devices/ad4080.xml
@@ -1,0 +1,1 @@
+<?xml version="1.0" encoding="utf-8"?><!DOCTYPE context [<!ELEMENT context (device | context-attribute)*><!ELEMENT context-attribute EMPTY><!ELEMENT device (channel | attribute | debug-attribute | buffer-attribute)*><!ELEMENT channel (scan-element?, attribute*)><!ELEMENT attribute EMPTY><!ELEMENT scan-element EMPTY><!ELEMENT debug-attribute EMPTY><!ELEMENT buffer-attribute EMPTY><!ATTLIST context name CDATA #REQUIRED description CDATA #IMPLIED><!ATTLIST context-attribute name CDATA #REQUIRED value CDATA #REQUIRED><!ATTLIST device id CDATA #REQUIRED name CDATA #IMPLIED><!ATTLIST channel id CDATA #REQUIRED type (input|output) #REQUIRED name CDATA #IMPLIED><!ATTLIST scan-element index CDATA #REQUIRED format CDATA #REQUIRED scale CDATA #IMPLIED><!ATTLIST attribute name CDATA #REQUIRED filename CDATA #IMPLIED value CDATA #IMPLIED><!ATTLIST debug-attribute name CDATA #REQUIRED value CDATA #IMPLIED><!ATTLIST buffer-attribute name CDATA #REQUIRED value CDATA #IMPLIED>]><context name="network" description="10.48.65.239 Linux analog 6.15.0-rc1-00274-gf1bd9f3b3978 #258 SMP PREEMPT Tue May 20 14:37:17 EEST 2025 armv7l" ><context-attribute name="hw_model" value="EVAL-AD4080-FMCZ on Xilinx Zynq ZED" /><context-attribute name="hw_carrier" value="Xilinx Zynq ZED" /><context-attribute name="hdl_system_id" value="[ad408x_fmc_evb] on [zed] git branch [dev_segura] git [97dda1eb9101a238f13f40991cd25452e6536611] dirty [2025-05-12 11:17:41] UTC" /><context-attribute name="hw_mezzanine" value="EVAL-AD4080-FMCZ" /><context-attribute name="hw_name" value="EVAL-AD4080-FMCZ" /><context-attribute name="hw_vendor" value="Analog Devices" /><context-attribute name="hw_serial" value="Empty Field" /><context-attribute name="local,kernel" value="6.15.0-rc1-00274-gf1bd9f3b3978" /><context-attribute name="uri" value="ip:10.48.65.239" /><context-attribute name="ip,ip-addr" value="10.48.65.239" /><device id="hwmon0" name="e000b000ethernetffffffff00" ><channel id="temp1" type="input" ><attribute name="crit" filename="temp1_crit" value="100000" /><attribute name="input" filename="temp1_input" value="41000" /><attribute name="max_alarm" filename="temp1_max_alarm" value="0" /></channel></device><device id="iio:device0" name="/axi/spi@e0007000/adf4350@1" ><channel id="altvoltage0" type="output" ><attribute name="frequency" filename="out_altvoltage0_frequency" value="400000000" /><attribute name="frequency_resolution" filename="out_altvoltage0_frequency_resolution" value="100000" /><attribute name="powerdown" filename="out_altvoltage0_powerdown" value="0" /><attribute name="refin_frequency" filename="out_altvoltage0_refin_frequency" value="25000000" /></channel><attribute name="waiting_for_supplier" value="0" /><debug-attribute name="direct_reg_access" value="0x400000" /></device><device id="iio:device1" name="ad9508" ><channel id="altvoltage3" name="MACH1_CLK" type="output" ><attribute name="frequency" filename="out_altvoltage3_MACH1_CLK_frequency" value="400000000" /><attribute name="label" filename="out_altvoltage3_MACH1_CLK_label" value="MACH1_CLK" /><attribute name="phase" filename="out_altvoltage3_MACH1_CLK_phase" value="0.000000" /><attribute name="raw" filename="out_altvoltage3_MACH1_CLK_raw" value="1" /></channel><channel id="altvoltage1" name="CLK10M" type="output" ><attribute name="frequency" filename="out_altvoltage1_CLK10M_frequency" value="10000000" /><attribute name="label" filename="out_altvoltage1_CLK10M_label" value="CLK10M" /><attribute name="phase" filename="out_altvoltage1_CLK10M_phase" value="0.000000" /><attribute name="raw" filename="out_altvoltage1_CLK10M_raw" value="1" /></channel><channel id="altvoltage2" name="CNV" type="output" ><attribute name="frequency" filename="out_altvoltage2_CNV_frequency" value="40000000" /><attribute name="label" filename="out_altvoltage2_CNV_label" value="CNV" /><attribute name="phase" filename="out_altvoltage2_CNV_phase" value="0.000000" /><attribute name="raw" filename="out_altvoltage2_CNV_raw" value="1" /></channel><channel id="altvoltage0" name="CNV_COPY" type="output" ><attribute name="frequency" filename="out_altvoltage0_CNV_COPY_frequency" value="40000000" /><attribute name="label" filename="out_altvoltage0_CNV_COPY_label" value="CNV_COPY" /><attribute name="phase" filename="out_altvoltage0_CNV_COPY_phase" value="0.000000" /><attribute name="raw" filename="out_altvoltage0_CNV_COPY_raw" value="1" /></channel><attribute name="sync_dividers" value="ERROR" /><attribute name="waiting_for_supplier" value="0" /><debug-attribute name="direct_reg_access" value="0x81" /></device><device id="iio:device2" name="ad4080" ><channel id="voltage0" type="input" ><scan-element index="0" format="le:s20/32&gt;&gt;0" scale="0.000006" /><attribute name="scale" filename="in_voltage0_scale" value="0.000005722" /></channel><attribute name="filter_type" value="none" /><attribute name="filter_type_available" value="none sinc1 sinc5 sinc5+pf1" /><attribute name="oversampling_ratio" value="1" /><attribute name="oversampling_ratio_available" value="1" /><attribute name="sampling_frequency" value="40000000" /><attribute name="waiting_for_supplier" value="0" /><buffer-attribute name="data_available" value="16384" /><buffer-attribute name="direction" value="in" /><buffer-attribute name="length_align_bytes" value="8" /><debug-attribute name="direct_reg_access" value="0x10" /></device></context>

test/emu/hardware_map.yml
@@ -721,3 +721,11 @@ ad738x:
       - data_devices:
           - iio:device0
 
+ad4080:
+ - ad4080
+ - pyadi_iio_class_support:
+     - ad4080
+ - emulate:
+     - filename: ad4080.xml
+     - data_devices:
+         - iio:device2

test/test_ad4080.py
@@ -1,0 +1,21 @@
+import pytest
+
+hardware = "ad4080"
+classname = "adi.ad4080"
+
+#########################################
+@pytest.mark.iio_hardware(hardware)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize(
+    "attr, val", [("filter_type", ["none", "sinc1", "sinc5", "sinc5+pf1"],),],
+)
+def test_ad4080_attr(test_attribute_multiple_values, iio_uri, classname, attr, val):
+    test_attribute_multiple_values(iio_uri, classname, attr, val, 0)
+
+
+#########################################
+@pytest.mark.iio_hardware(hardware, True)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0])
+def test_ad4080_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel)

commit 086d5cd8011375ebf10885d7773656a73a8642df

ad4080: fix scale to read per-channel attribute
ad4080_channel.scale read a hardcoded "voltage0" and therefore returned
channel 0's scale for every channel. Use the actual channel name so each
channel reports its own scale.

Fixes: 47913405e07a ("ad4080: add pyadi-iio support")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

adi/ad4080.py
@@ -16,7 +16,7 @@ class ad4080_channel(attribute):
     @property
     def scale(self):
         """Get Scale value"""
-        return self._get_iio_attr("voltage0", "scale", False)
+        return self._get_iio_attr(self.name, "scale", False)
 
 
 class ad4080(rx_chan_comp):