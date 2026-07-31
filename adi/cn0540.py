0a9671199683e5dd47ea0bfd5b87b146385d17e4
Marcelo Schmitt <marcelo.schmitt@analog.com>
2026-07-31 10:32:46 -0300

    adi: cn0540: Re-add calibration procedure
    CN0540 calibration procedure was removed in February, 2022. However,
    costumers reported missing that feature in August, 2023. The same commit
    that removed the calibrate function from CN0540 also added a slightly
    updated version of the calibrate function to CN0532. Re-introduce the
    calibration feature to CN0540 on same updated form that is available in
    CN0532.
    
    Link: https://ez.analog.com/linux-software-drivers/f/q-a/572902/trouble-calibrating-cn0540-using-pyadi-iio?queryID=411c40aa86081da8ea48e1c6bad51858&objectId=82ecfad5-7016-4bd9-b5f2-74ffcddb4450&eztype=Forum%20Thread
    
    Fixes: eaad1fabe809 ("[cn0540] Update calibration routine")
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- adi/cn0540.py
+++ adi/cn0540.py
@@ -49,6 +49,43 @@ class cn0540(rx_def):
         self._gpio = self._ctx.find_device("one-bit-adc-dac")
         self._ltc2308 = self._ctx.find_device("ltc2308")
 
+    @reset_buffer
+    def calibrate(self):
+        """Tune the LTC2606 until the AD7768-1 ADC codes approach zero mean."""
+        adc_chan = self._rxadc
+        dac_chan = self._ltc2606
+        adc_scale = float(self._get_iio_attr("voltage0", "scale", False, adc_chan))
+        dac_scale = float(self._get_iio_attr("voltage0", "scale", True, dac_chan))
+
+        for _ in range(20):
+            raw = self._get_iio_attr("voltage0", "raw", False, adc_chan)
+            adc_voltage = raw * adc_scale
+
+            raw = self._get_iio_attr("voltage0", "raw", True, dac_chan)
+            dac_voltage = (raw * dac_scale - adc_voltage) / dac_scale
+
+            e = int(dac_voltage * dac_scale)
+
+            if int(dac_voltage) > 2 ** 16 - 1:
+                print(
+                    "Warning: DAC voltage at upper limit, "
+                    + f"calibration may not converge (Error: {e - (2**16 - 1)} codes).\n"
+                    + "Make sure sensor is connected."
+                )
+                dac_voltage = 2 ** 16 - 1
+            elif int(dac_voltage) < 0:
+                print(
+                    "Warning: DAC voltage at lower limit, "
+                    + f"calibration may not converge (Error: {e} codes).\n"
+                    + "Make sure sensor is connected."
+                )
+                dac_voltage = 0
+
+            self._set_iio_attr_float(
+                "voltage0", "raw", True, int(dac_voltage), dac_chan
+            )
+            time.sleep(0.01)
+
     @property
     def sample_rate(self):
         """Sample rate in samples per second."""
